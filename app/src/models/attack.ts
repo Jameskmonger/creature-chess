8bc074fb3487ee798ccf2a1014a3ca3b5506257c
Tom Hyde <thyde@neworbit.co.uk>
2019-03-15 23:41:14 +0000

    Pokemon have a "cooldown timer" to reflect their speed

--- app/src/models/attack.ts
+++ app/src/models/attack.ts
@@ -1,12 +1,17 @@
-import { PokemonPiece } from "./pokemon-piece";
+import { PokemonPiece, initialCoolDown } from "./pokemon-piece";
 import { getPokemonStats } from "./get-pokemon-stats";
 import { getTypeAttackBonus } from "./get-type-attack-bonus";
 
 export const attack = (attacker: PokemonPiece, defender: PokemonPiece) => {
     const attackerStats = getPokemonStats(attacker.pokemonId);
+    if (attacker.coolDown > 0) {
+        return { attacker: { ...attacker, coolDown: attacker.coolDown - attackerStats.speed }, defender };
+    }
+
     const defenderStats = getPokemonStats(defender.pokemonId);
     const attackBonus = getTypeAttackBonus(attackerStats.type, defenderStats.type);
-    const damage = (attackerStats.attack / defenderStats.defense) * attackBonus;
+    const damage = (attackerStats.attack / defenderStats.defense) * attackBonus * 10;
     const newDefenderHealth = Math.max(defender.currentHealth - damage, 0);
-    return { attacker, defender: { ...defender, currentHealth: newDefenderHealth } };
+
+    return { attacker: { ...attacker, coolDown: initialCoolDown }, defender: { ...defender, currentHealth: newDefenderHealth } };
 };
