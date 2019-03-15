a3448918fe62fad76be528e15dcef304a2c7a4e3
Tom Hyde <thyde@neworbit.co.uk>
2019-03-15 23:44:48 +0000

    Dead Pokémon don't attack

--- app/src/models/attack.ts
+++ app/src/models/attack.ts
@@ -3,6 +3,11 @@ import { getPokemonStats } from "./get-pokemon-stats";
 import { getTypeAttackBonus } from "./get-type-attack-bonus";
 
 export const attack = (attacker: PokemonPiece, defender: PokemonPiece) => {
+    if (attacker.currentHealth === 0) {
+        // Dead Pokémon don't attack
+        return { attacker, defender };
+    }
+
     const attackerStats = getPokemonStats(attacker.pokemonId);
     const defenderStats = getPokemonStats(defender.pokemonId);
     const attackBonus = getTypeAttackBonus(attackerStats.type, defenderStats.type);
