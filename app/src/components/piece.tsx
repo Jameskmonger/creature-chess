52101ed76d824e309c99a5fd01ad1f512b65d213
Tom Hyde <thyde@neworbit.co.uk>
2019-03-15 23:07:14 +0000

    Comment properly

--- app/src/components/piece.tsx
+++ app/src/components/piece.tsx
@@ -29,8 +29,8 @@ const PieceUnconnected: React.FunctionComponent<PieceProps & DragSourceProps> = 
 
             <div className="info">
                 <div className={`healthbar ${friendly ? "friendly" : "enemy"}`}>
-                    // need to figure out how we handle setting the healthbar
-                    // tslint:disable-next-line:jsx-ban-props
+                    {/* need to figure out how we handle setting the healthbar */}
+                    {/* tslint:disable-next-line:jsx-ban-props */}
                     <div className="fill" style={{ width: getPercentage(currentHealth, maxHealth) }} />
                 </div>
             </div>
