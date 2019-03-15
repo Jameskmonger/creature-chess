c44a776c16ca2396da642513a4d843581e7631cc
Tom Hyde <thyde@neworbit.co.uk>
2019-03-15 21:37:00 +0000

    Use existing piece objects rather than cloning

--- app/src/models/fighting-turn-simulator.ts
+++ app/src/models/fighting-turn-simulator.ts
@@ -1,9 +1,9 @@
 import { PokemonPiece } from './pokemon-piece';
-import { sample, cloneDeep } from "lodash";
+import { sample } from "lodash";
 import { attack } from './attack';
 
 export const simulateTurn = (pieces: PokemonPiece[]) => {
-    const updatedPieces = cloneDeep(pieces);
+    const updatedPieces = [...pieces];
     updatedPieces.forEach((attacker, index) => {
         const defender = sample(updatedPieces.filter(p => p.friendly !== attacker.friendly && p.currentHealth > 0));
         if (!defender) {
