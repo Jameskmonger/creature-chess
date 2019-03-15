bd87dcb126d1a50aa236cddbc262def95887d8bf
Tom Hyde <thyde@neworbit.co.uk>
2019-03-15 23:07:26 +0000

    Get real stats

--- app/src/models/get-pokemon-stats.ts
+++ app/src/models/get-pokemon-stats.ts
@@ -1,4 +1,3 @@
-import { PokemonType } from "./pokemon-type";
 import { pokemonStats } from "./pokemon-stats";
 
 export const getPokemonStats = (pokemonId: number) => pokemonStats.find(p => p.id === pokemonId).stats;
