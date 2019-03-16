8a523a211671ab996017fb0ceefc2bc0867622f6
James Monger <jameskmonger@hotmail.co.uk>
2019-03-16 11:00:18 +0000

    Choose a target to move towards

--- app/src/models/fighting-turn-simulator.ts
+++ app/src/models/fighting-turn-simulator.ts
@@ -1,13 +1,20 @@
 import { PokemonPiece } from "./pokemon-piece";
 import { attack } from "./attack";
-import { getAttackableEnemy } from "./movement";
+import { getAttackableEnemy, getNewPiecePosition } from "./movement";
 
 export const simulateTurn = (pieces: PokemonPiece[]) => {
     const updatedPieces: PokemonPiece[] = pieces.map(p => ({ ...p, attacking: false, hit: false }));
+
     updatedPieces.forEach((attacker, index) => {
         const defender = getAttackableEnemy(attacker, updatedPieces);
 
         if (!defender) {
+            const newPosition = getNewPiecePosition(attacker, updatedPieces);
+
+            if (newPosition !== null) {
+                updatedPieces[index].position = newPosition;
+            }
+
             return updatedPieces;
         }
 
@@ -15,5 +22,6 @@ export const simulateTurn = (pieces: PokemonPiece[]) => {
         updatedPieces[index] = updatedFighters.attacker;
         updatedPieces[updatedPieces.indexOf(defender)] = updatedFighters.defender;
     });
+
     return updatedPieces;
 };
--- app/src/models/movement.ts
+++ app/src/models/movement.ts
@@ -1,16 +1,46 @@
 import { PokemonPiece } from "./pokemon-piece";
 
+const getLivingEnemies = (piece: PokemonPiece, pieces: PokemonPiece[]) => {
+    return pieces.filter(other => other.friendly !== piece.friendly && other.currentHealth > 0);
+};
+
+const getDelta = (a: PokemonPiece, b: PokemonPiece) => {
+    return {
+        x: Math.abs(a.position[0] - b.position[0]),
+        y: Math.abs(a.position[1] - b.position[1])
+    };
+};
+
 const isAdjacent = (a: PokemonPiece) => {
     return (b: PokemonPiece) => {
-        const deltaX = Math.abs(a.position[0] - b.position[0]);
-        const deltaY = Math.abs(a.position[1] - b.position[1]);
+        const { x: deltaX, y: deltaY } = getDelta(a, b);
 
         return (deltaX + deltaY === 1);
     };
 };
 
 export const getAttackableEnemy = (piece: PokemonPiece, others: PokemonPiece[]) => {
-    const enemies = others.filter(other => other.friendly !== piece.friendly && other.currentHealth > 0);
+    return getLivingEnemies(piece, others).find(isAdjacent(piece)) || null;
+};
 
-    return enemies.find(isAdjacent(piece)) || null;
+const getTargetPiece = (piece: PokemonPiece, pieces: PokemonPiece[]) => {
+    const enemies = getLivingEnemies(piece, pieces);
+
+    const enemyDeltas = enemies.map(enemy => ({
+        enemy,
+        delta: getDelta(piece, enemy)
+    }));
+
+    // sort by column then by row
+    enemyDeltas.sort((a, b) => a.delta.y - b.delta.y || a.delta.x - b.delta.x);
+
+    return enemyDeltas[0].enemy;
+};
+
+export const getNewPiecePosition = (piece: PokemonPiece, pieces: PokemonPiece[]): [ number, number ] => {
+    const target = getTargetPiece(piece, pieces);
+
+
+
+    return null;
 };
