af117922d64afca88dbfe099ac763510d47c254c
Tom Hyde <thyde@neworbit.co.uk>
2019-03-17 18:56:18 +0000

    Refactored runAnimation

--- app/src/components/piece.tsx
+++ app/src/components/piece.tsx
@@ -21,7 +21,11 @@ interface DragSourceProps {
 
 interface Animation {
     name: string;
-    variables?: { [key: string]: string | number };
+    variables?: AnimationVariables;
+}
+
+interface AnimationVariables {
+    [key: string]: string | number;
 }
 
 interface State {
@@ -76,28 +80,28 @@ class PieceUnconnected extends React.Component<PieceProps & DragSourceProps, Sta
     private runAnimations = (oldProps: PieceProps) => {
         const { moving, attacking, hit, celebrating, currentHealth } = this.props.piece;
         if (!oldProps.piece.moving && moving) {
-            this.runAnimation({ name: `move-${moving.direction}` });
+            this.runAnimation(`move-${moving.direction}`);
         }
 
         if (!oldProps.piece.attacking && attacking) {
-            this.runAnimation({ name: `attack-${attacking.direction}`, variables: { attackPower: attacking.damage } });
+            this.runAnimation(`attack-${attacking.direction}`, { attackPower: attacking.damage });
         }
 
         if (!oldProps.piece.hit && hit) {
-            this.runAnimation({ name: "hit", variables: { hitPower: hit.damage } });
+            this.runAnimation("hit", { hitPower: hit.damage });
         }
 
         if (!oldProps.piece.celebrating && celebrating) {
-            this.runAnimation({ name: "celebrate" });
+            this.runAnimation("celebrate");
         }
 
         if (oldProps.piece.currentHealth > 0 && currentHealth === 0) {
-            this.runAnimation({ name: dyingAnimation });
+            this.runAnimation(dyingAnimation);
         }
     }
 
-    private runAnimation = (animation: Animation) => {
-        this.setState((prevState => ({ ...prevState, currentAnimation: animation })));
+    private runAnimation = (name: string, variables?: AnimationVariables) => {
+        this.setState((prevState => ({ ...prevState, currentAnimation: { name, variables } })));
     }
 
     private onAnimationEnd = (event: React.AnimationEvent<HTMLDivElement>) => {
