5908cef8455c239870473f4f3d285e23c3f0dfae
James Monger <jameskmonger@hotmail.co.uk>
2019-03-20 22:58:11 +0000

    Wire up socket.io client

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
 // tslint:disable:no-console
 import io = require("socket.io");
-import { CardDeck } from "@common/cardDeck";
+import { CardDeck } from "../shared/cardDeck";
 import { Player } from "./player";
 
 const server = io.listen(3000);
@@ -17,14 +17,17 @@ server.on("connection", socket => {
     });
 
     socket.on("shuffleCards", () => {
+        console.log("shuffling cards");
+        console.log("IDs before are: " + JSON.stringify(player.cards.map(p => p.id)));
         // prevent any race conditions
         const playerCards = player.cards;
         player.cards = [];
 
-        deck.add(player.cards);
+        deck.add(playerCards);
         deck.shuffle();
 
         player.cards = deck.take(5);
+        console.log("IDs after are: " + JSON.stringify(player.cards.map(p => p.id)));
 
         socket.emit("cardsUpdate", player.cards);
     });
