52320c8db2b1a8212bff19d2ad3565bb5e56875d
James Monger <jameskmonger@hotmail.co.uk>
2019-03-21 13:31:43 +0000

    Create fake opponent and wire up board update packet

--- src/server/player.ts
+++ src/server/player.ts
@@ -1,5 +1,8 @@
 import { PokemonCard } from "../shared";
+import { PokemonPiece } from "../shared/pokemon-piece";
 
 export interface Player {
     cards: PokemonCard[];
+    board: PokemonPiece[];
+    opponent?: Player;
 }
