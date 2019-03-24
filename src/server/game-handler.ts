5c2ac20d72bbf3529c0e8128dd997e0308d24f2c
James Monger <jameskmonger@hotmail.co.uk>
2019-03-24 21:13:32 +0000

    Update game-handler.ts

--- src/server/game-handler.ts
+++ src/server/game-handler.ts
@@ -30,7 +30,11 @@ export class GameHandler {
                 makeFriendly(129, [4, 4]),
                 makeFriendly(70, [7, 6]),
                 makeFriendly(67, [3, 4]),
-                makeFriendly(89, [5, 4])
+                makeFriendly(89, [5, 4]),
+                
+                makeFriendly(9, [8, 2], true),
+                makeFriendly(70, [8, 5], true),
+                makeFriendly(67, [8, 6], true)
             ],
             opponent
         };
