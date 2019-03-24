1f659ef269562acfec6e334885bd3aa83f63a028
Tom Hyde <thyde@neworbit.co.uk>
2019-03-24 15:28:57 +0000

    Cards look a little cardier

--- src/app/components/selectedPieceInfo/selectedPieceInfoPanel.tsx
+++ src/app/components/selectedPieceInfo/selectedPieceInfoPanel.tsx
@@ -3,13 +3,14 @@ import { PokemonPiece } from "@common/pokemon-piece";
 import { connect, MapStateToProps } from "react-redux";
 import { AppState } from "../../store/store";
 import { SelectedPieceInfo } from "./selectedPieceInfo";
+import { getPokemonStats } from "@common/pokemon-details";
 
 interface Props {
     piece: PokemonPiece;
 }
 
 const SelectedPieceInfoPanelUnconnected: React.FunctionComponent<Props> = (props) => (
-    <div>
+    <div className={`selected-piece-info-panel card ${props.piece ? getPokemonStats(props.piece.pokemonId).type.toLowerCase() : ""}`}>
         {props.piece && <SelectedPieceInfo />}
     </div>
 );
