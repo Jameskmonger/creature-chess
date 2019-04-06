a40cbcac7f1300b115ac834885e6ccd5797fbf6e
James Monger <jameskmonger@hotmail.co.uk>
2019-04-06 18:31:43 +0100

    Split into columns

--- src/app/components/stages/game-stage.tsx
+++ src/app/components/stages/game-stage.tsx
@@ -14,6 +14,7 @@ import { CardSelector } from "../cardSelector";
 import { AppState } from "../../store/store";
 import { SelectedPieceInfoPanel } from "../selectedPieceInfo/selectedPieceInfoPanel";
 import { piecesUpdated } from "../../actions/pieceActions";
+import { PlayerList } from "../playerList/playerList";
 
 const isATeamDefeated = (pieces: PokemonPiece[]) => {
     return !(pieces.some(p => p.friendly && p.currentHealth > 0) && pieces.some(p => !p.friendly && p.currentHealth > 0));
@@ -58,7 +59,7 @@ class GameStageUnconnected extends React.Component<Props, GameStageState> {
         });
 
         this.socket.on("boardUpdate", (packet: { friendly: PokemonPiece[], opponent: PokemonPiece[] }) => {
-            const pieces = [ ...packet.friendly, ...packet.opponent ];
+            const pieces = [...packet.friendly, ...packet.opponent];
             this.props.onPiecesUpdated(pieces);
         });
     }
@@ -73,16 +74,40 @@ class GameStageUnconnected extends React.Component<Props, GameStageState> {
 
         return (
             <div className="game">
+                <div className="column">
+                    <PlayerList
+                        players={[
+                            {
+                                id: "1",
+                                name: "Owen",
+                                health: 100
+                            },
+                            {
+                                id: "3",
+                                name: "James",
+                                health: 65
+                            },
+                            {
+                                id: "4",
+                                name: "Tom",
+                                health: 83
+                            },
+                            {
+                                id: "2",
+                                name: "Henry",
+                                health: 22
+                            }
+                        ]}
+                    />
+                </div>
                 <div className="board-container" style={boardContainerStyle}>
                     <div className="chessboard">
                         <Board boardSize={boardSize} />
                         <Bench boardSize={boardSize} pieces={benchPieces} />
                     </div>
-                    <SelectedPieceInfoPanel />
                 </div>
-                <div className="shop-container">
-                    <CardSelector cards={cards} onShuffle={this.onShuffle} />
-                    <button onClick={(this.startRound)}>Fight!</button>
+                <div className="column">
+
                 </div>
             </div>
         );
