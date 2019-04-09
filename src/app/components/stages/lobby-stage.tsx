381509c5e188ca9fb38725d52038988a6ffa18c0
James Monger <jameskmonger@hotmail.co.uk>
2019-04-09 22:11:17 +0100

    Use entered name to join

--- src/app/components/stages/lobby-stage.tsx
+++ src/app/components/stages/lobby-stage.tsx
@@ -6,24 +6,43 @@ interface DispatchProps {
     onJoinGame: (name: string) => void;
 }
 
+interface LobbyStageProps {
+    name: string;
+}
+
 type Props = DispatchProps;
 
 class LobbyStageUnconnected extends React.Component<Props> {
+    public state = {
+        name: ""
+    };
+
     public render() {
         return (
             <div className="lobby">
                 <div className="join-game">
                     <h2 className="title">Pokemon Auto Chess</h2>
 
-                    <input placeholder="Your name" className="name-input" />
+                    <input
+                        value={this.state.name}
+                        onChange={this.onNameChange}
+                        placeholder="Your name"
+                        className="name-input"
+                    />
                     <button onClick={this.onJoinGameClick} className="join-button">Join Game</button>
                 </div>
             </div>
         );
     }
 
+    private onNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
     private onJoinGameClick = () => {
-        this.props.onJoinGame("Player");
+        this.props.onJoinGame(this.state.name);
     }
 }
 
