dffc3244209a34724939aefcc563f6d928f61704
James Monger <jameskmonger@hotmail.co.uk>
2019-04-10 13:28:50 +0100

    Update playerListActions.ts

--- src/app/actions/playerListActions.ts
+++ src/app/actions/playerListActions.ts
@@ -3,7 +3,7 @@ import { PLAYER_LIST_UPDATED } from "../actiontypes/playerListActionTypes";
 
 export type PlayerListAction = ({ type: PLAYER_LIST_UPDATED, payload: PlayerListPlayer[] });
 
-export const playerListUpdated = (payload: PlayerListPlayer[]) => { console.log(payload); return ({
+export const playerListUpdated = (payload: PlayerListPlayer[]) => ({
     type: PLAYER_LIST_UPDATED,
     payload
-}) };
+});
