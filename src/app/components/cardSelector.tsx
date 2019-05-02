593a7ebf91000a3fe86298880942b4e2ce0edba4
James Monger <jameskmonger@hotmail.co.uk>
2019-05-02 17:49:33 +0100

    Wire up cardSelector to redux

--- src/app/components/cardSelector.tsx
+++ src/app/components/cardSelector.tsx
@@ -2,31 +2,48 @@ import * as React from "react";
 import { PokemonCard } from "@common";
 import { take } from "lodash";
 import { Card, RerollCard } from "./card";
+import { MapStateToProps, connect, MapDispatchToProps } from "react-redux";
+import { AppState } from "../store/store";
+import { ClientToServerPacketOpcodes } from "../../shared/packet-opcodes";
+import { sendPacket } from "../actions/networkActions";
 
-interface CardSelectorProps {
+interface StateProps {
     cards: PokemonCard[];
     money: number;
+}
+
+interface DispatchProps {
     onShuffle: () => void;
 }
 
-class CardSelector extends React.Component<CardSelectorProps> {
+type Props = StateProps & DispatchProps;
 
-    public render() {
-        const { cards, onShuffle, money } = this.props;
-        const topCards = take(cards, 5);
+const CardSelectorUnconnected: React.FunctionComponent<Props> = props => {
+    const { cards, onShuffle, money } = props;
+    const topCards = take(cards, 5);
 
-        return (
-            <div className="card-selector">
-                <div className="balance">Balance: ${money}</div>
-                <div className="cards">
-                    {topCards.map((card, index) => <Card key={`${index}-${card.id}`} pokemonId={card.id} cost={card.cost} name={card.name} />)}
+    return (
+        <div className="card-selector">
+            <div className="balance">Balance: ${money}</div>
+            <div className="cards">
+                {topCards.map((card, index) => <Card key={`${index}-${card.id}`} pokemonId={card.id} cost={card.cost} name={card.name} />)}
 
-                    <RerollCard onClick={onShuffle} />
-                </div>
+                <RerollCard onClick={onShuffle} />
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+const mapStateToProps: MapStateToProps<StateProps, {}, AppState> = state => ({
+    cards: state.cards,
+    money: state.game.money
+});
+
+const mapDispatchToProps: MapDispatchToProps<DispatchProps, {}> = dispatch => ({
+    onShuffle: () => dispatch(sendPacket(ClientToServerPacketOpcodes.REFRESH_CARDS))
+});
+
+const CardSelector = connect(mapStateToProps, mapDispatchToProps)(CardSelectorUnconnected);
 
 export {
     CardSelector
