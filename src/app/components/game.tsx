cee021d440fc1ec0591239dfb28026d603ea7151
James Monger <jameskmonger@hotmail.co.uk>
2019-05-03 22:08:58 +0100

    Rename GameContainer -> Game

--- src/app/components/game.tsx
+++ src/app/components/game.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 import { MapStateToProps, connect, MapDispatchToProps } from "react-redux";
-import { GameStage } from "./stages/game-stage";
+import { GameStage } from "./stages/gameStage";
 import { AppState } from "../store/store";
-import { LobbyStage } from "./stages/lobby-stage";
+import { LobbyStage } from "./stages/lobbyStage";
 import { localPlayerIdSelector } from "../selectors/gameSelector";
 
 interface Props {
     inLobby: boolean;
 }
 
-class GameContainerUnconnected extends React.Component<Props, {}> {
+class GameUnconnected extends React.Component<Props, {}> {
     public render() {
         const { inLobby } = this.props;
 
@@ -25,8 +25,8 @@ const mapStateToProps: MapStateToProps<Props, {}, AppState> = state => ({
     inLobby: localPlayerIdSelector(state) === null
 });
 
-const GameContainer = connect(mapStateToProps)(GameContainerUnconnected);
+const Game = connect(mapStateToProps)(GameUnconnected);
 
 export {
-    GameContainer
+    Game
 };
