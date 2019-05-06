76cf9a8dc5739b671388b36ab7ae2180f1859dca
James Monger <jameskmonger@hotmail.co.uk>
2019-05-06 21:32:28 +0100

    Make game size an argument

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,7 +1,6 @@
 import { GameState } from "./game-state";
 
 export const GRID_SIZE = 8;
-export const MAX_PLAYER_COUNT = 2;
 export const STATE_LENGTHS = {
     [GameState.PREPARING]: 30,
     [GameState.READY]: 5
