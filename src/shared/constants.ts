643c58c2c450d91f35d45fcc4f092e13afc21bac
James Monger <jameskmonger@hotmail.co.uk>
2019-05-07 17:43:54 +0100

    Cap turn simulation count on server

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -4,6 +4,9 @@ export const GRID_SIZE = 8;
 export const PHASE_LENGTHS = {
     [GamePhase.PREPARING]: 30,
     [GamePhase.READY]: 5,
-    [GamePhase.PLAYING]: 20
+    [GamePhase.PLAYING]: 30
 };
 export const REROLL_COST = 2;
+
+export const TURN_DURATION_MS = 50;
+export const TURNS_IN_BATTLE = Math.floor((PHASE_LENGTHS[GamePhase.PLAYING] * 1000) / TURN_DURATION_MS);
