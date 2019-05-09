d48df6efc595256ccbc3f8dfd3af33f21c65926d
James Monger <jameskmonger@hotmail.co.uk>
2019-05-09 21:46:41 +0100

    Set money to 3

--- src/server/players/player.ts
+++ src/server/players/player.ts
@@ -24,7 +24,7 @@ export class Player {
     private cards: PokemonCard[] = [];
     private board: PokemonPiece[] = [];
     private bench: PokemonPiece[] = [];
-    private money: number = 50;
+    private money: number = 3;
     private health: number = 100;
     private match: Match = null;
     private streak = {
