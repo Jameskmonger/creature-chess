32518c5d8be9c9af2df6fee342442546396221ec
James Monger <jameskmonger@hotmail.co.uk>
2019-05-18 00:31:18 +0100

    Fix ready phase time

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -3,7 +3,7 @@ import { GamePhase } from "./game-phase";
 export const GRID_SIZE = 8;
 export const PHASE_LENGTHS = {
     [GamePhase.PREPARING]: 30,
-    [GamePhase.READY]: 1,
+    [GamePhase.READY]: 5,
     [GamePhase.PLAYING]: 30
 };
 export const REROLL_COST = 2;
