e96c240d20231ca80927f9ccd0243eca19fc4679
James Monger <jameskmonger@hotmail.co.uk>
2019-05-18 02:38:06 +0100

    Filter battle messages when moving to ready state

--- src/app/reducers/feedMessagesReducer.ts
+++ src/app/reducers/feedMessagesReducer.ts
@@ -1,12 +1,20 @@
 import { FeedAction } from "../actions/feedActions";
-import { FeedMessage } from "@common/feed-message";
+import { FeedMessage, FeedMessageType } from "@common/feed-message";
 import { NEW_FEED_MESSAGE } from "../actiontypes/feedActionTypes";
-import { ChatMessageAction } from "../actions/chatActions";
+import { GamePhaseUpdateAction } from "../actions/gameActions";
+import { GAME_PHASE_UPDATE } from "../actiontypes/gameActionTypes";
+import { GamePhase } from "../../shared";
 
-export const feedMessages = (state: FeedMessage[] = [], action: FeedAction | ChatMessageAction) => {
+export const feedMessages = (state: FeedMessage[] = [], action: FeedAction | GamePhaseUpdateAction) => {
     switch (action.type) {
         case NEW_FEED_MESSAGE:
             return [action.payload, ...state];
+        case GAME_PHASE_UPDATE:
+            if (action.payload.phase !== GamePhase.READY) {
+                return state;
+            }
+
+            return state.filter(m => m.type !== FeedMessageType.BATTLE);
         default:
             return state;
     }
