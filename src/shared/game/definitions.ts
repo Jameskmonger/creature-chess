96e2fb49e865943d95d7d8bc611d9f618cac1027
James Monger <jameskmonger@hotmail.co.uk>
2019-06-05 00:50:12 +0100

    Create balanced stats2

--- src/shared/game/definitions.ts
+++ src/shared/game/definitions.ts
@@ -8,22 +8,22 @@ export const definitions = [
         type: CreatureType.Forest,
         stages: [
             {
-                hp: 45,
-                attack: 49,
-                defense: 49,
-                speed: 45
+                hp: 30,
+                attack: 50,
+                defense: 50,
+                speed: 30
             },
             {
-                hp: 60,
-                attack: 62,
-                defense: 63,
-                speed: 60
+                hp: 40,
+                attack: 60,
+                defense: 60,
+                speed: 40
             },
             {
-                hp: 80,
-                attack: 82,
-                defense: 83,
-                speed: 80
+                hp: 50,
+                attack: 70,
+                defense: 70,
+                speed: 50
             }
         ]
     },
@@ -34,22 +34,22 @@ export const definitions = [
         type: CreatureType.Fire,
         stages: [
             {
-                hp: 39,
-                attack: 52,
-                defense: 43,
-                speed: 65
+                hp: 30,
+                attack: 50,
+                defense: 50,
+                speed: 30
             },
             {
-                hp: 58,
-                attack: 64,
-                defense: 58,
-                speed: 80
+                hp: 40,
+                attack: 60,
+                defense: 60,
+                speed: 40
             },
             {
-                hp: 78,
-                attack: 84,
-                defense: 78,
-                speed: 100
+                hp: 50,
+                attack: 70,
+                defense: 70,
+                speed: 50
             }
         ]
     },
@@ -60,22 +60,22 @@ export const definitions = [
         type: CreatureType.Water,
         stages: [
             {
-                hp: 44,
-                attack: 48,
-                defense: 65,
-                speed: 43
+                hp: 30,
+                attack: 50,
+                defense: 50,
+                speed: 30
             },
             {
-                hp: 59,
-                attack: 63,
-                defense: 80,
-                speed: 58
+                hp: 40,
+                attack: 60,
+                defense: 60,
+                speed: 40
             },
             {
-                hp: 79,
-                attack: 83,
-                defense: 100,
-                speed: 78
+                hp: 50,
+                attack: 70,
+                defense: 70,
+                speed: 50
             }
         ]
     },
@@ -86,22 +86,22 @@ export const definitions = [
         type: CreatureType.Forest,
         stages: [
             {
-                hp: 45,
-                attack: 30,
-                defense: 35,
-                speed: 45
+                hp: 30,
+                attack: 10,
+                defense: 30,
+                speed: 40
             },
             {
-                hp: 50,
+                hp: 40,
+                attack: 25,
+                defense: 40,
+                speed: 55
+            },
+            {
+                hp: 100,
                 attack: 20,
-                defense: 55,
-                speed: 30
-            },
-            {
-                hp: 60,
-                attack: 45,
-                defense: 50,
-                speed: 70
+                defense: 100,
+                speed: 20
             }
         ]
     },
@@ -112,22 +112,22 @@ export const definitions = [
         type: CreatureType.Forest,
         stages: [
             {
+                hp: 20,
+                attack: 30,
+                defense: 25,
+                speed: 30
+            },
+            {
+                hp: 25,
+                attack: 40,
+                defense: 35,
+                speed: 30
+            },
+            {
                 hp: 40,
-                attack: 35,
-                defense: 30,
-                speed: 50
-            },
-            {
-                hp: 45,
-                attack: 25,
-                defense: 50,
-                speed: 35
-            },
-            {
-                hp: 65,
-                attack: 90,
+                attack: 70,
                 defense: 40,
-                speed: 75
+                speed: 80
             }
         ]
     },
@@ -138,22 +138,22 @@ export const definitions = [
         type: CreatureType.Fire,
         stages: [
             {
-                hp: 37,
-                attack: 53,
-                defense: 42,
-                speed: 68
+                hp: 30,
+                attack: 50,
+                defense: 50,
+                speed: 30
             },
             {
-                hp: 59,
-                attack: 66,
-                defense: 71,
-                speed: 81
+                hp: 40,
+                attack: 60,
+                defense: 60,
+                speed: 40
             },
             {
-                hp: 81,
-                attack: 87,
-                defense: 79,
-                speed: 120
+                hp: 50,
+                attack: 70,
+                defense: 70,
+                speed: 50
             }
         ]
     },
@@ -164,22 +164,22 @@ export const definitions = [
         type: CreatureType.Forest,
         stages: [
             {
-                hp: 55,
-                attack: 47,
-                defense: 52,
-                speed: 41
+                hp: 20,
+                attack: 40,
+                defense: 20,
+                speed: 40
             },
             {
-                hp: 70,
-                attack: 62,
-                defense: 67,
-                speed: 56
+                hp: 30,
+                attack: 50,
+                defense: 30,
+                speed: 55
             },
             {
-                hp: 90,
-                attack: 92,
-                defense: 87,
-                speed: 76
+                hp: 40,
+                attack: 90,
+                defense: 30,
+                speed: 90
             }
         ]
     },
@@ -190,22 +190,22 @@ export const definitions = [
         type: CreatureType.Forest,
         stages: [
             {
+                hp: 40,
+                attack: 25,
+                defense: 40,
+                speed: 25
+            },
+            {
                 hp: 55,
-                attack: 52,
-                defense: 47,
-                speed: 40
+                attack: 35,
+                defense: 55,
+                speed: 35
             },
             {
                 hp: 70,
-                attack: 67,
-                defense: 62,
-                speed: 55
-            },
-            {
-                hp: 90,
-                attack: 87,
-                defense: 92,
-                speed: 75
+                attack: 50,
+                defense: 90,
+                speed: 40
             }
         ]
     }
