c3e50c5490ce8ed9e7e4bf41ae05888e13464abd
James Monger <jameskmonger@hotmail.co.uk>
2019-08-28 12:52:28 +0100

    Fix image links

--- src/app/components/creatureImage.tsx
+++ src/app/components/creatureImage.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 const CreatureImage: React.FunctionComponent<Props> = ({ facing, definitionId, stage }) => (
-    <img className="image" src={`/images/${facing || "front"}/${definitionId}_${stage}.png`} />
+    <img className="image" src={`images/${facing || "front"}/${definitionId}_${stage}.png`} />
 );
 
 export { CreatureImage };
