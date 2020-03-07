247fd2933679d83a33a58545a567e613c73454b0
James Monger <jameskmonger@hotmail.co.uk>
2020-03-07 11:59:52 +0000

    Check for empty name before running regex

--- src/server/name-validator.ts
+++ src/server/name-validator.ts
@@ -3,6 +3,10 @@ import { MAX_NAME_LENGTH } from '@common/constants';
 const NAME_REGEX = /^[a-zA-Z0-9_\ ]*$/;
 
 export const nameValidator = (name: string) => {
+    if (!name) {
+        return "No name provided";
+    }
+
     if (name.match(NAME_REGEX) === null) {
         return "Invalid characters in name";
     }
