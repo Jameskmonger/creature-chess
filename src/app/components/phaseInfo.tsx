8311bf9a26a9e9b47389d910e5ee95ef8ee7049e
James Monger <jameskmonger@hotmail.co.uk>
2020-03-07 21:44:46 +0000

    Use React hooks to display countdown timers (#230)
    * Use React hooks to display countdown timers
    
    * Use JoinLobby action to remove saga

--- src/app/components/phaseInfo.tsx
+++ src/app/components/phaseInfo.tsx
@@ -1,14 +1,36 @@
 import * as React from "react";
-import { GamePhase } from "@common";
+import { GamePhase, Constants } from "@common";
 import { MapStateToProps, connect } from "react-redux";
 import { AppState } from "../store/state";
 
 interface Props {
     phase: GamePhase;
-    phaseTimer: number;
+    phaseStartedAtSeconds: number;
 }
 
-const PhaseInfoUnconnected: React.FunctionComponent<Props> = ({ phase, phaseTimer }) => {
+const PhaseInfoUnconnected: React.FunctionComponent<Props> = ({ phase, phaseStartedAtSeconds }) => {
+    const [phaseTimeRemaining, setPhaseTimeRemaining] = React.useState<number | null>(null);
+
+    const updateTimeRemaining = () => {
+        if (phaseStartedAtSeconds === null) {
+            return;
+        }
+
+        const currentSeconds = Math.floor(Date.now() / 1000);
+        const phaseEndTime = Constants.PHASE_LENGTHS[phase] + phaseStartedAtSeconds;
+
+        setPhaseTimeRemaining(phaseEndTime - currentSeconds);
+    };
+
+    React.useEffect(() => {
+        updateTimeRemaining();
+
+        const intervalId = setInterval(updateTimeRemaining, 1000);
+
+        return () => clearInterval(intervalId);
+
+    }, [phase, phaseStartedAtSeconds]);
+
     if (phase === GamePhase.WAITING) {
         return <div className="phase-info">Waiting for players</div>;
     }
@@ -17,12 +39,12 @@ const PhaseInfoUnconnected: React.FunctionComponent<Props> = ({ phase, phaseTime
         return <div className="phase-info">You are dead</div>;
     }
 
-    return <div className="phase-info">{GamePhase[phase]} - {phaseTimer}</div>;
+    return <div className="phase-info">{GamePhase[phase]} - {phaseTimeRemaining}</div>;
 };
 
 const mapStateToProps: MapStateToProps<Props, {}, AppState> = state => ({
     phase: state.game.phase,
-    phaseTimer: state.game.phaseTimer
+    phaseStartedAtSeconds: state.game.phaseStartedAtSeconds
 });
 
 const PhaseInfo = connect(mapStateToProps)(PhaseInfoUnconnected);
