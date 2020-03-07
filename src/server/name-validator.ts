98d449731c54f4a8cc83eefc8a36dd6b5b0c2e8e
James Monger <jameskmonger@hotmail.co.uk>
2020-03-07 12:06:24 +0000

    Add log for name validator

--- src/server/name-validator.ts
+++ src/server/name-validator.ts
@@ -1,8 +1,11 @@
 import { MAX_NAME_LENGTH } from '@common/constants';
+import { log } from '@common/log';
 
 const NAME_REGEX = /^[a-zA-Z0-9_\ ]*$/;
 
 export const nameValidator = (name: string) => {
+    log(`Attempting to validate name: ${JSON.stringify(name)}`);
+
     if (!name) {
         return "No name provided";
     }
