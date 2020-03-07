dc998ef7d3e9e514a42dfd5f7eecbcd7553db7d9
James Monger <jameskmonger@hotmail.co.uk>
2020-03-07 17:33:53 +0000

    Fix getPiecesForStage

--- src/shared/get-pieces-for-stage.ts
+++ src/shared/get-pieces-for-stage.ts
@@ -1,13 +1,13 @@
 export const getPiecesForStage = (stage: number) => {
-    if (stage === 1) {
+    if (stage === 0) {
         return 1;
     }
 
-    if (stage === 2) {
+    if (stage === 1) {
         return 3;
     }
 
-    if (stage === 3) {
+    if (stage === 2) {
         return 9;
     }
 
