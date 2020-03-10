527af2f41efdea89d601607d76cc1b983cd6f2cc
James Monger <jameskmonger@hotmail.co.uk>
2020-03-10 23:17:29 +0000

    Only start game sagas on JOIN_COMPLETE

--- src/app/store/sagas/index.ts
+++ src/app/store/sagas/index.ts
@@ -1,4 +1,4 @@
-import { fork, all } from "@redux-saga/core/effects";
+import { fork, all, takeEvery } from "@redux-saga/core/effects";
 import { networking } from "./actions/networking";
 import { phaseTimer } from "./actions/phaseTimer";
 import { gamePhase } from "./actions/gamePhase";
@@ -11,16 +11,29 @@ import { DefinitionProvider } from "@common/game/definitionProvider";
 import { DEFAULT_TURN_COUNT, DEFAULT_TURN_DURATION } from "@common/constants";
 import { AppState } from "../state";
 import { announcement } from "./actions/announcement";
+import { JOIN_COMPLETE } from "../actiontypes/localPlayerActionTypes";
 
 export const rootSaga = function*() {
     yield all([
-        yield fork(networking),
-        yield fork(phaseTimer),
-        yield fork(announcement),
-        yield fork(gamePhase),
         yield fork(preventAccidentalClose),
-        yield fork(cardShop),
-        yield fork(evolutionSagaFactory<AppState>()),
-        yield fork(battle, new TurnSimulator(new DefinitionProvider()), DEFAULT_TURN_COUNT, DEFAULT_TURN_DURATION)
+        yield fork(networking),
+        yield takeEvery(
+            JOIN_COMPLETE,
+            function*() {
+                yield all([
+                    yield fork(phaseTimer),
+                    yield fork(announcement),
+                    yield fork(gamePhase),
+                    yield fork(cardShop),
+                    yield fork(evolutionSagaFactory<AppState>()),
+                    yield fork(
+                        battle,
+                        new TurnSimulator(new DefinitionProvider()),
+                        DEFAULT_TURN_COUNT,
+                        DEFAULT_TURN_DURATION
+                    )
+                ]);
+            }
+        )
     ]);
 };
