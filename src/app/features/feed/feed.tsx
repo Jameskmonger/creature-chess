801abbcae24f406c32688933970d06401b4c9eff
James Monger <jameskmonger@hotmail.co.uk>
2020-03-31 21:03:03 +0100

    Remove battle score feed

--- src/app/features/feed/feed.tsx
+++ src/app/features/feed/feed.tsx
@@ -1,25 +1,17 @@
 import * as React from "react";
-import { FeedMessage, BattleFeedMessage, ChatFeedMessage, FeedMessageType } from "@common/models/feed-message";
+import { FeedMessage } from "@common/models/feed-message";
 import { connect, MapStateToProps } from "react-redux";
 import { AppState } from "@app/store";
 import { ChatInput } from "../chat/chatInput";
 
 interface FeedStateProps {
     messages: FeedMessage[];
-    getMessageAuthor: (message: ChatFeedMessage) => string;
+    getMessageAuthor: (message: FeedMessage) => string;
 }
 
-const BattleFeedMessage: React.FunctionComponent<{ message: BattleFeedMessage }> = ({ message }) => (
-    <p className="battle-feed-message">
-        <span className="player-name home">{message.home}</span> <span className="score">{message.homeScore}</span>
-        <span className="vs"> vs </span>
-        <span className="score">{message.awayScore}</span> <span className="player-name away">{message.away}</span>
-    </p>
-);
-
 const ChatFeedMessage: React.FunctionComponent<{
-    message: ChatFeedMessage,
-    getMessageAuthor: (message: ChatFeedMessage) => string;
+    message: FeedMessage,
+    getMessageAuthor: (message: FeedMessage) => string;
 }> = ({ message, getMessageAuthor }) => (
     <p className="chat-feed-message">
         <span className="sender">{getMessageAuthor(message)}: </span>
@@ -32,13 +24,7 @@ const FeedUnconnected: React.FunctionComponent<FeedStateProps> = ({ messages, ge
         <div className="feed">
             <ChatInput />
             {messages.map(message => {
-                if (message.type === FeedMessageType.BATTLE) {
-                    return <BattleFeedMessage key={message.id} message={message.payload} />;
-                }
-
-                if (message.type === FeedMessageType.CHAT) {
-                    return <ChatFeedMessage key={message.id} message={message.payload} getMessageAuthor={getMessageAuthor} />;
-                }
+                return <ChatFeedMessage key={message.id} message={message} getMessageAuthor={getMessageAuthor} />;
             })}
         </div>
     );
