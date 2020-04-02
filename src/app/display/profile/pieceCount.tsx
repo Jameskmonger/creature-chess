45acfe4be37f7ad56c8b54e3b8586cd1a2a789ba
James Monger <jameskmonger@hotmail.co.uk>
2020-04-02 23:28:20 +0100

    Display piece count warning

--- src/app/display/profile/pieceCount.tsx
+++ src/app/display/profile/pieceCount.tsx
@@ -7,6 +7,10 @@ const PieceCount: React.FunctionComponent = props => {
   const level = useSelector<AppState, number>(state => state.localPlayer.level);
   const pieceCount = useSelector<AppState, number>(state => ownedPieceSelector(state).length);
 
+  if (pieceCount !== level) {
+    return <p className="pieces warning">{pieceCount} / {level} pieces (board not full!)</p>;
+  }
+
   return <p className="pieces">{pieceCount} / {level} pieces</p>;
 };
 
