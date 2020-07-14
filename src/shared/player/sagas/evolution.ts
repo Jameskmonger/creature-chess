13ea4e465770d88ecfe3ce1e636ddc3c05694353
James Monger <jameskmonger@hotmail.co.uk>
2020-07-14 22:59:10 +0100

    Fix #267: Check for evolutions after UPDATE_BOARD_PIECE

--- src/shared/player/sagas/evolution.ts
+++ src/shared/player/sagas/evolution.ts
@@ -6,9 +6,9 @@ import { ADD_BENCH_PIECE } from "../bench/benchActionTypes";
 import { DefinitionProvider } from "@common/game/definitionProvider";
 import { PieceModel } from "@common/models";
 import { PIECES_TO_EVOLVE } from "@common/models/constants";
-import { UNLOCK_BOARD } from "@common/board/actions/boardActionTypes";
+import { UNLOCK_BOARD, UPDATE_BOARD_PIECE } from "@common/board/actions/boardActionTypes";
 import * as pieceSelectors from "../pieceSelectors";
-import { removeBoardPieces, updateBoardPiece } from "@common/board/actions/boardActions";
+import { removeBoardPieces, updateBoardPiece, UpdateBoardPieceAction } from "@common/board/actions/boardActions";
 
 const definitionProvider = new DefinitionProvider();
 
@@ -17,80 +17,86 @@ interface State {
     board: BoardState;
 }
 
+const pieceCanEvolve = (piece: PieceModel) => {
+    const { stages } = definitionProvider.get(piece.definitionId);
+
+    return piece.stage < stages.length - 1;
+};
+
 export const evolutionSagaFactory = <TState extends State>() => {
     return function*() {
-        yield all([
-            yield takeLatest<AddBenchPieceAction>(
-                ADD_BENCH_PIECE,
-                function*({ payload: { piece } }) {
-                    const { stages } = definitionProvider.get(piece.definitionId);
-
-                    const nextStageIndex = piece.stage + 1;
-                    const nextStage = stages[nextStageIndex];
-
-                    if (!nextStage) {
-                        return;
-                    }
-
-                    const boardLocked: boolean = yield select((s: TState) => s.board.locked);
-
-                    // if evolution is locked, wait for it to be unlocked
-                    if (boardLocked) {
-                        yield take(UNLOCK_BOARD);
-                        yield delay(500);
-                    }
-
-                    const getCombinablePieces = (pieces: PieceModel[]) => pieces.filter(p => p.id !== piece.id && p.stage === piece.stage);
-
-                    const state: TState = yield select();
-
-                    const matchingBoardPieces = getCombinablePieces(
-                        pieceSelectors.getBoardPiecesForDefinition(state, piece.definitionId)
-                    );
-                    const matchingBenchPieces = getCombinablePieces(
-                        pieceSelectors.getBenchPiecesForDefinition(state, piece.definitionId)
-                    );
-
-                    const totalInstances = matchingBoardPieces.length + matchingBenchPieces.length + 1;
-
-                    if (totalInstances < PIECES_TO_EVOLVE) {
-                        return;
-                    }
-
-                    if (matchingBoardPieces.length > 0) {
-                        // replace a board piece if it exists
-                        const pieceToReplace = matchingBoardPieces.pop();
-
-                        // remove any remaining board pieces
-                        const boardPieceIds = matchingBoardPieces.map(p => p.id);
-                        yield put(removeBoardPieces(boardPieceIds));
-
-                        const benchPieceIds = matchingBenchPieces.map(p => p.id);
-                        yield put(removeBenchPieces([ ...benchPieceIds, piece.id ]));
-
-                        const newPiece = {
-                            ...pieceToReplace,
-                            stage: nextStageIndex
-                        };
-
-                        yield put(updateBoardPiece(newPiece));
-                    } else {
-                        // otherwise replace the just-added bench piece
-                        const benchPieceIds = matchingBenchPieces.map(p => p.id);
-                        yield put(removeBenchPieces(benchPieceIds));
-
-                        const newPiece = {
-                            ...piece,
-                            stage: nextStageIndex
-                        };
-
-                        // todo make updateBenchPiece action
-                        yield put(removeBenchPiece(piece.id));
-                        yield put(addBenchPiece(newPiece, null));
-                    }
+        yield takeLatest<AddBenchPieceAction | UpdateBoardPieceAction>(
+            // need to check when bench pieces are added (could have come from shop)
+            // or when board piece is updated (could be due to a previous evolution)
+            [ADD_BENCH_PIECE, UPDATE_BOARD_PIECE],
+            function*({ payload: { piece } }) {
+                if (!pieceCanEvolve(piece)) {
+                    return;
                 }
-            )
-        ]);
+
+                const boardLocked: boolean = yield select((s: TState) => s.board.locked);
+
+                // if evolution is locked, wait for it to be unlocked
+                if (boardLocked) {
+                    // todo check if we have 3 evolvable pieces on the bench and evolve those? maybe
+
+                    yield take(UNLOCK_BOARD);
+                    yield delay(500);
+                }
+
+                const state: TState = yield select();
+
+                const targetDefinitionId = piece.definitionId;
+                const targetStage = piece.stage;
+
+                const getCombinablePieces = (pieces: PieceModel[]) => pieces.filter(p => p.stage === targetStage);
+
+                const matchingBoardPieces = getCombinablePieces(
+                    pieceSelectors.getBoardPiecesForDefinition(state, targetDefinitionId)
+                );
+                const matchingBenchPieces = getCombinablePieces(
+                    pieceSelectors.getBenchPiecesForDefinition(state, targetDefinitionId)
+                );
+
+                const totalInstances = matchingBoardPieces.length + matchingBenchPieces.length;
+
+                if (totalInstances < PIECES_TO_EVOLVE) {
+                    return;
+                }
+
+                if (matchingBoardPieces.length > 0) {
+                    // replace a board piece if it exists
+                    const pieceToReplace = matchingBoardPieces.pop();
+
+                    // remove any remaining board pieces
+                    const boardPieceIds = matchingBoardPieces.map(p => p.id);
+                    yield put(removeBoardPieces(boardPieceIds));
+
+                    const benchPieceIds = matchingBenchPieces.map(p => p.id);
+                    yield put(removeBenchPieces([...benchPieceIds, piece.id]));
+
+                    const newPiece = {
+                        ...pieceToReplace,
+                        stage: targetStage + 1
+                    };
+
+                    yield put(updateBoardPiece(newPiece));
+                } else {
+                    // otherwise replace the just-added bench piece
+                    const benchPieceIds = matchingBenchPieces.map(p => p.id);
+                    yield put(removeBenchPieces(benchPieceIds));
+
+                    const newPiece = {
+                        ...piece,
+                        stage: targetStage + 1
+                    };
+
+                    // todo make updateBenchPiece action
+                    yield put(removeBenchPiece(piece.id));
+                    yield put(addBenchPiece(newPiece, null));
+                }
+            }
+        );
     };
 
 };
