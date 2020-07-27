d7ec86be82f6af646cc175ee94b922b4b4aef4c6
James Monger <jameskmonger@hotmail.co.uk>
2020-07-27 23:43:07 +0100

    Target enemies with higher cost if there's a tie break

--- src/shared/match/combat/movement.ts
+++ src/shared/match/combat/movement.ts
@@ -34,7 +34,7 @@ const getAttackingTiles = (facingUp: boolean, attackType: AttackType) => {
 const getLivingEnemies = (piece: PieceModel, board: BoardState): PieceModel[] => {
     const output: PieceModel[] = [];
 
-    for (const [ id, other ] of Object.entries(board.pieces)) {
+    for (const [id, other] of Object.entries(board.pieces)) {
         if (other.ownerId !== piece.ownerId && other.currentHealth > 0) {
             output.push(other);
         }
@@ -116,7 +116,29 @@ const findClosestEnemy = (piece: PieceModel, board: BoardState) => {
     }));
 
     // sort by column then by row
-    enemyDeltas.sort((a, b) => a.delta.y - b.delta.y || a.delta.x - b.delta.x);
+    enemyDeltas.sort((a, b) => {
+        if (a.delta.y < b.delta.y) {
+            return -1;
+        }
+
+        if (a.delta.y > b.delta.y) {
+            return 1;
+        }
+
+        if (a.delta.x < b.delta.x) {
+            return -1;
+        }
+
+        if (a.delta.x > b.delta.x) {
+            return 1;
+        }
+
+        if (a.enemy.definition.cost > b.enemy.definition.cost) {
+            return -1;
+        }
+
+        return 1;
+    });
 
     return enemyDeltas[0].enemy;
 };
