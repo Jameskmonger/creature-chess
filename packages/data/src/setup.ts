f33aaa836fbb4bd2ba2f6d4ee71021ff812992ca
James Monger <jameskmonger@hotmail.co.uk>
2020-09-14 17:33:32 +0100

    Setup database on initialisation (#301)
    * Remove outdir for /data/
    
    * Setup database on initialisation

---
+++ packages/data/src/setup.ts
@@ -0,0 +1,45 @@
+import { Client as FaunaDBClient, query as q } from "faunadb";
+import { COLLECTION_NAMES, INDEX_NAMES } from "./constants";
+
+const INSTANCE_ALREADY_EXISTS = "instance already exists";
+
+export const setup = async (client: FaunaDBClient) => {
+    console.log("Setting up database...");
+
+    try {
+        await client.query(q.CreateCollection({
+            name: COLLECTION_NAMES.USERS
+        }));
+
+        console.log(` - Created collection '${COLLECTION_NAMES.USERS}'`);
+    } catch (e) {
+        if (e.message === INSTANCE_ALREADY_EXISTS) {
+            console.log(` - Colletion '${COLLECTION_NAMES.USERS}' already exists`);
+        } else {
+            throw e
+        }
+    }
+
+    try {
+        await client.query(q.CreateIndex({
+            name: INDEX_NAMES.USERS_BY_NICKNAME_UPPERCASE,
+            serialized: true,
+            unique: true,
+            source: q.Collection(COLLECTION_NAMES.USERS),
+            terms: [
+                {
+                    // todo type safety around this - is it even possible?
+                    field: ["data", "nickname", "uppercase"]
+                }
+            ]
+        }))
+
+        console.log(` - Created index '${INDEX_NAMES.USERS_BY_NICKNAME_UPPERCASE}'`);
+    } catch (e) {
+        if (e.message === INSTANCE_ALREADY_EXISTS) {
+            console.log(` - Index '${INDEX_NAMES.USERS_BY_NICKNAME_UPPERCASE}' already exists`);
+        } else {
+            throw e
+        }
+    }
+}
