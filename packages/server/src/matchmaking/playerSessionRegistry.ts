2e3e2b5a29bba8558cba27af42f6c89cfe6734d3
James Monger <jameskmonger@hotmail.co.uk>
2020-10-01 23:45:10 +0100

    Create registerGame function on PlayerSessionRegistry

--- packages/server/src/matchmaking/playerSessionRegistry.ts
+++ packages/server/src/matchmaking/playerSessionRegistry.ts
@@ -1,4 +1,6 @@
-import { Player } from "@creature-chess/shared/game";
+import { log } from "@creature-chess/shared";
+import { Game, Player } from "@creature-chess/shared/game";
+import { SocketPlayer } from "../player/socketPlayer";
 
 type PlayerLocation = {
     type: "lobby" | "game",
@@ -14,7 +16,7 @@ export class PlayerSessionRegistry {
     } = {};
 
     public registerPlayer(playerId: string, player: Player, type: "lobby" | "game", id: string) {
-        this.sessions[playerId] = { player, location: { type, id }};
+        this.sessions[playerId] = { player, location: { type, id } };
     }
 
     public deregisterPlayer(playerId: string) {
@@ -23,5 +25,35 @@ export class PlayerSessionRegistry {
 
     public getPlayer(playerId: string) {
         return this.sessions[playerId] || null;
+    }
+
+    public registerGame(game: Game) {
+        game.getPlayers()
+            .filter(p => (p as SocketPlayer).isConnection)
+            .forEach(p => {
+                this.registerPlayer(p.id, p, "game", game.id);
+            });
+
+        game.onFinish((winner, gamePlayers) => {
+            gamePlayers.forEach(p => {
+                if (!p.isBot) {
+                    this.deregisterPlayer(p.id);
+                }
+            });
+        });
+
+        game.onPlayerDeath(p => {
+            if (!p.isBot) {
+                this.deregisterPlayer(p.id);
+            }
+        });
+
+        game.onPlayerQuit(p => {
+            log(`Player '${p.name}' quit game ${game.id}`);
+
+            if (!p.isBot) {
+                this.deregisterPlayer(p.id);
+            }
+        });
     }
 }
