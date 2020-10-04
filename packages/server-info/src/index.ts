a5f79e83c4c88afa865d5db26eebdaaead15318b
James Monger <jameskmonger@hotmail.co.uk>
2020-10-04 23:27:08 +0100

    Store leaderboard players in a cache

--- packages/server-info/src/index.ts
+++ packages/server-info/src/index.ts
@@ -13,8 +13,38 @@ app.use((req, res, next) => {
     next();
 });
 
+const createLeaderboardCache = () => {
+    const TIME_LIVE = 120 * 1000;
+    let cachedValue = null;
+    let cacheValidUntil = null;
+
+    return async () => {
+        const currentTime = Date.now();
+        if (cachedValue && currentTime < cacheValidUntil) {
+            console.log("Retrieved cached leaderboard value");
+            return cachedValue;
+        }
+
+        console.log("Getting new value from database");
+
+        const users = await database.leaderboard.getPlayers();
+
+        if (users) {
+            console.log("Setting new cached value");
+            cachedValue = users;
+            cacheValidUntil = Date.now() + TIME_LIVE;
+            return users;
+        }
+
+        console.log("Falling back to cached value");
+
+        return cachedValue;
+    };
+};
+const getLeaderboardPlayers = createLeaderboardCache();
+
 app.get("/leaderboard", async (req, res) => {
-    const users = await database.leaderboard.getPlayers();
+    const users = await getLeaderboardPlayers();
 
     res.send(users);
 });
