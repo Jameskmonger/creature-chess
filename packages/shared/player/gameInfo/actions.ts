1f8c4c6d568b26dcb4efa16c12bfaf80a2e09ee3
James Monger <jameskmonger@hotmail.co.uk>
2020-10-05 23:37:25 +0100

    [refactor] Merge level state into gameinfo

--- packages/shared/player/gameInfo/actions.ts
+++ packages/shared/player/gameInfo/actions.ts
@@ -8,18 +8,28 @@ export const SET_OPPONENT = "SET_OPPONENT";
 export type SET_OPPONENT = typeof SET_OPPONENT;
 export const CLEAR_OPPONENT = "CLEAR_OPPONENT";
 export type CLEAR_OPPONENT = typeof CLEAR_OPPONENT;
+export const LEVEL_UPDATE = "LEVEL_UPDATE";
+export type LEVEL_UPDATE = typeof LEVEL_UPDATE;
 
 export type UpdateShopLockAction = ({ type: SHOP_LOCK_UPDATED, payload: { locked: boolean } });
 export type MoneyUpdateAction = ({ type: MONEY_UPDATE, payload: { money: number } });
 export type SetOpponentAction = ({ type: SET_OPPONENT, payload: { opponentId: string } });
 export type ClearOpponentAction = ({ type: CLEAR_OPPONENT });
+export type LevelUpdateAction = ({
+    type: LEVEL_UPDATE;
+    payload: {
+        level: number;
+        xp: number;
+    };
+});
 
 export type GameAction =
     UpdateShopLockAction
     | MoneyUpdateAction
     | ReadyUpAction
     | SetOpponentAction
-    | ClearOpponentAction;
+    | ClearOpponentAction
+    | LevelUpdateAction;
 
 export const shopLockUpdated = (locked: boolean): UpdateShopLockAction => ({
     type: SHOP_LOCK_UPDATED,
@@ -35,3 +45,10 @@ export const moneyUpdateAction = (money: number): MoneyUpdateAction => ({
 
 export const setOpponent = (opponentId: string): SetOpponentAction => ({ type: SET_OPPONENT, payload: { opponentId } });
 export const clearOpponent = (): ClearOpponentAction => ({ type: CLEAR_OPPONENT });
+
+export const setLevelAction = (level: number, xp: number): LevelUpdateAction => ({
+    type: LEVEL_UPDATE,
+    payload: {
+        level, xp
+    }
+});
