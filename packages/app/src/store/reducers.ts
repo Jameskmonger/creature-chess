8f4793c020edafb832e2ec4917f61ad50f2fbe54
James Monger <jameskmonger@hotmail.co.uk>
2020-10-05 23:48:09 +0100

    [refactor] Flatten reducers

--- packages/app/src/store/reducers.ts
+++ packages/app/src/store/reducers.ts
@@ -1,11 +1,11 @@
 import { boardReducer } from "@creature-chess/shared/board";
 import { benchReducer } from "@creature-chess/shared/player/bench";
 import { gameInfoReducer } from "@creature-chess/shared/player/gameInfo";
-import { playerList } from "../../game/features/playerList/playerListReducer";
 import { gameReducer } from "@creature-chess/shared/game";
-import { authReducer } from "../../auth";
-import { lobbyReducer } from "../../lobby";
-import { uiReducer } from "../../ui";
+import { playerList } from "../game/features/playerList/playerListReducer";
+import { authReducer } from "../auth";
+import { lobbyReducer } from "../lobby";
+import { uiReducer } from "../ui";
 
 export const reducers = {
     board: boardReducer,
