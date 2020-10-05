ecfa0ca67838ab1078cec907af8d94d1e39fde44
James Monger <jameskmonger@hotmail.co.uk>
2020-10-05 19:14:22 +0100

    Move debug and connectionStatus to UI state

--- packages/app/src/game/store/state.ts
+++ packages/app/src/game/store/state.ts
@@ -1,8 +1,4 @@
-import { ConnectionStatus } from "@creature-chess/shared/networking";
-
 export interface GameState {
     id: string;
     round: number | null;
-    debug: boolean;
-    connectionStatus: ConnectionStatus;
 }
--- packages/app/src/store/actions/uiActions.ts
+++ packages/app/src/store/actions/uiActions.ts
@@ -1,3 +1,4 @@
+import { ConnectionStatus } from "@creature-chess/shared/networking";
 import { Overlay } from "../../game/overlay";
 
 export const OPEN_OVERLAY = "OPEN_OVERLAY";
@@ -8,11 +9,17 @@ export const UPDATE_ANNOUNCEMENT = "UPDATE_ANNOUNCEMENT";
 export type UPDATE_ANNOUNCEMENT = typeof UPDATE_ANNOUNCEMENT;
 export const CLEAR_ANNOUNCEMENT = "CLEAR_ANNOUNCEMENT";
 export type CLEAR_ANNOUNCEMENT = typeof CLEAR_ANNOUNCEMENT;
+export const ENABLE_DEBUG_MODE = "ENABLE_DEBUG_MODE";
+export type ENABLE_DEBUG_MODE = typeof ENABLE_DEBUG_MODE;
+export const UPDATE_CONNECTION_STATUS = "UPDATE_CONNECTION_STATUS";
+export type UPDATE_CONNECTION_STATUS = typeof UPDATE_CONNECTION_STATUS;
 
 export type OpenOverlayAction = { type: OPEN_OVERLAY, payload: { overlay: Overlay } };
 export type CloseOverlayAction = { type: CLOSE_OVERLAY };
 export type AnnouncementUpdateAction = ({ type: UPDATE_ANNOUNCEMENT, payload: { main: string, sub?: string } });
 export type AnnouncementClearAction = ({ type: CLEAR_ANNOUNCEMENT });
+export type EnableDebugModeAction = ({ type: ENABLE_DEBUG_MODE });
+export type UpdateConnectionStatusAction = ({ type: UPDATE_CONNECTION_STATUS, payload: { status: ConnectionStatus } });
 
 export const openOverlay = (overlay: Overlay): OpenOverlayAction => ({
     type: OPEN_OVERLAY,
@@ -31,3 +38,12 @@ export const updateAnnouncement = (main: string, sub?: string): AnnouncementUpda
 });
 
 export const clearAnnouncement = (): AnnouncementClearAction => ({ type: CLEAR_ANNOUNCEMENT });
+
+export const enableDebugMode = (): EnableDebugModeAction => ({ type: ENABLE_DEBUG_MODE });
+
+export const updateConnectionStatus = (status: ConnectionStatus): UpdateConnectionStatusAction => ({
+    type: UPDATE_CONNECTION_STATUS,
+    payload: {
+        status
+    }
+});
