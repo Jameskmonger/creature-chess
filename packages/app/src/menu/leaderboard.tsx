d905c449d6906114bf216549bc3a9066e4b067d8
James Monger <jameskmonger@hotmail.co.uk>
2020-10-13 23:28:19 +0100

    Style leaderboard and add wipe warning

--- packages/app/src/menu/leaderboard.tsx
+++ packages/app/src/menu/leaderboard.tsx
@@ -3,23 +3,31 @@ import useFetch from "use-http";
 
 const LeaderboardContents: React.FunctionComponent<{ data: any }> = ({ data }) => {
     return (
-        <table className="leaderboard">
-            <thead>
-                <tr>
-                    <td>Player</td>
-                    <td>Wins</td>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    data.map(
-                        ({ wins, name }, index) => (
-                            <tr key={`${index}-${name}`}><td>{name}</td><td>{wins}</td></tr>
+        <>
+            <table className="leaderboard">
+                <thead>
+                    <tr>
+                        <td>Player</td>
+                        <td>Wins</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map(
+                            ({ wins, name }, index) => (
+                                <tr key={`${index}-${name}`}><td>{name}</td><td>{wins}</td></tr>
+                            )
                         )
-                    )
-                }
-            </tbody>
-        </table>
+                    }
+                </tbody>
+            </table>
+
+            <div className="wipe-warning">
+                <p>Thanks so much for playing! I hope you're enjoying the game</p>
+                <p>Please don't get too attached to these scores for now</p>
+                <p>The user accounts will be wiped during development</p>
+            </div>
+        </>
     );
 };
 
