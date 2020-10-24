debbf2ec7c056d458c186519968185c478a9549b
James Monger <jameskmonger@hotmail.co.uk>
2020-10-24 20:38:48 +0100

    Add GAME_SERVER_URL

--- packages/app/src/auth/config.ts
+++ packages/app/src/auth/config.ts
@@ -7,5 +7,6 @@ export const logoutRedirectUrl = local ? "http://localhost:8090" : "http://creat
 export const audience = `https://${domain}/userinfo`;
 export const scope = "openid profile email";
 
+export const GAME_SERVER_URL = "https://cc-server.jamesmonger.com";
 export const CURRENT_USER_ENDPOINT = local ? "http://localhost:3001/user/current" : "https://cc-server-info.herokuapp.com/user/current";
 export const LEADERBOARD_ENDPOINT = local ? "http://localhost:3001/leaderboard" : "https://cc-server-info.herokuapp.com/leaderboard";
