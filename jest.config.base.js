1cffcd51ad3f9d19fe08478614f3ef09d569c001
James Monger <jameskmonger@hotmail.co.uk>
2020-10-25 02:17:29 +0000

    Add Jest config

--- jest.config.base.js
+++ jest.config.base.js
@@ -3,7 +3,7 @@ module.exports = {
         "<rootDir>/src"
     ],
     transform: {
-        "^.+\\.ts$": "ts-jest"
+        "^.+\.ts$": "ts-jest"
     },
     testRegex: "(\/src\/.*\/.*\.test\.tsx?)$",
     moduleFileExtensions: [
---
+++ packages/shared/jest.config.js
@@ -0,0 +1,8 @@
+// Jest configuration for api
+const base = require("../../jest.config.base.js");
+
+module.exports = {
+    ...base,
+    name: "@creature-chess/shared",
+    displayName: "@creature-chess/shared"
+};
