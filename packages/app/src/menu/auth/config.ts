7087b8657925e26fd921d7c7c98a45186af36d28
James Monger <jameskmonger@hotmail.co.uk>
2021-01-21 01:12:12 +0000

    fix: Fix callback url

--- packages/app/src/menu/auth/config.ts
+++ packages/app/src/menu/auth/config.ts
@@ -3,7 +3,7 @@ const local = false;
 export const auth0Config = {
     domain: "creaturechess.eu.auth0.com",
     clientID: "HNUYYyRCtFJsA3xKGp964Kgy4jwx8bW1",
-    redirectUri: local ? "https://creaturechess.local-dev.com:8090" : "http://creaturechess.jamesmonger.com/callback",
+    redirectUri: local ? "https://creaturechess.local-dev.com:8090" : "http://creaturechess.jamesmonger.com/",
     logoutRedirectUri: local ? "https://creaturechess.local-dev.com:8090" : "http://creaturechess.jamesmonger.com/",
     audience: `https://creaturechess.eu.auth0.com/userinfo`,
     scope: "openid profile email"
