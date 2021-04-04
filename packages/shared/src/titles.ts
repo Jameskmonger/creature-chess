d4be497d92a0db5fa6e75fdd0b58d2126581cc8e
James Monger <jameskmonger@hotmail.co.uk>
2021-04-04 15:16:18 +0100

    chore: add "hall of fame" title

--- packages/shared/src/titles.ts
+++ packages/shared/src/titles.ts
@@ -1,6 +1,8 @@
 const TITLES = {
-    DEVELOPER: { className: "developer-001", text: "Developer" }
+    DEVELOPER: { className: "developer-001", text: "Developer" },
+    HALL_OF_FAME: { className: "hall-of-fame-001", text: "Hall of Fame" }
 };
 export const PLAYER_TITLES: { [playerId: string]: { className: string, text: string } } = {
-    "276389458988761607": TITLES.DEVELOPER
+    "276389458988761607": TITLES.DEVELOPER,
+    "276850371065807367": TITLES.HALL_OF_FAME
 };
