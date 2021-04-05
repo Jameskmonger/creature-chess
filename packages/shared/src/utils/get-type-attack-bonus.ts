65c2e7e530d8287fd59c14c722a696d301b03c73
James Monger <jameskmonger@hotmail.co.uk>
2021-04-05 22:23:27 +0100

    feat: rework attack bonus [trial]

--- packages/shared/src/utils/get-type-attack-bonus.ts
+++ packages/shared/src/utils/get-type-attack-bonus.ts
@@ -28,15 +28,18 @@ export const isGeneratedBy = (defender: CreatureType, attacker: CreatureType): b
 export const isOvercomeBy = (defender: CreatureType, attacker: CreatureType): boolean => typeInteractions[defender].overcomeBy === attacker;
 
 export const getTypeAttackBonus = (attackType: CreatureType, defenceType: CreatureType) => {
-    // an attack is weak against the element that it Generates and strong against the element that it Overcomes.
-    const defenderInteractions = typeInteractions[defenceType];
+    // an attack is strong against the element it overcomes, and weak against things that overcome the attacker
 
-    if (defenderInteractions.generatedBy === attackType) {
-        return 0.7;
-    }
+    const defenderInteractions = typeInteractions[defenceType];
 
     if (defenderInteractions.overcomeBy === attackType) {
         return 1.4;
+    }
+
+    const attackerInteractions = typeInteractions[defenceType];
+
+    if (attackerInteractions.overcomeBy === defenceType) {
+        return 0.7;
     }
 
     return 1;
