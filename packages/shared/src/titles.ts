efee03a3348e79abe0220869e282b0b4cf0898b6
James Monger <jameskmonger@hotmail.co.uk>
2021-04-05 01:58:17 +0100

    chore: add contributor title

--- packages/shared/src/titles.ts
+++ packages/shared/src/titles.ts
@@ -1,8 +1,10 @@
 const TITLES = {
     DEVELOPER: { className: "developer-001", text: "Developer" },
+    CONTRIBUTOR: { className: "contributor-001", text: "Contributor" },
     HALL_OF_FAME: { className: "hall-of-fame-001", text: "Hall of Fame" }
 };
 export const PLAYER_TITLES: { [playerId: string]: { className: string, text: string } } = {
     "276389458988761607": TITLES.DEVELOPER,
+    "289896988988670470": TITLES.CONTRIBUTOR,
     "276850371065807367": TITLES.HALL_OF_FAME
 };
