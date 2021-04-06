5d9b9c7e408dec261ff6d1520dc1b38878a6a075
James Monger <jameskmonger@hotmail.co.uk>
2021-04-06 18:35:42 +0100

    refactor: use BoardOverlay for victory screen

--- packages/app/src/game/features/board/overlays/victoryOverlay.tsx
+++ packages/app/src/game/features/board/overlays/victoryOverlay.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useSelector } from "react-redux";
 import { AppState } from "../../../../store";
+import { BoardOverlay } from "./boardOverlay";
 
 const VictoryOverlay: React.FunctionComponent = () => {
     const winnerName = useSelector<AppState, string>(state => state.ui.winnerName);
@@ -10,16 +11,18 @@ const VictoryOverlay: React.FunctionComponent = () => {
     }
 
     return (
-        <div className="victory">
-            <h2 className="game-over">Game Over</h2>
-            <p><span className="winner">{winnerName}</span> wins!</p>
+        <BoardOverlay>
+            <div className="victory-overlay">
+                <h2 className="game-over">Game Over</h2>
+                <p className="winner"><span className="highlight">{winnerName}</span> wins!</p>
 
-            <div className="discord-link">
-                <p>Join us on Discord to receive notifications when someone starts a lobby, and more!</p>
+                <div className="discord-link">
+                    <p>Join us on Discord to receive notifications when someone starts a lobby, and more!</p>
 
-                <a href="https://discord.gg/FhMm6saehb"><img src="https://i.imgur.com/YNyTNuw.png" className="discord-button" /></a>
+                    <a href="https://discord.gg/FhMm6saehb"><img src="https://i.imgur.com/YNyTNuw.png" className="discord-button" /></a>
+                </div>
             </div>
-        </div >
+        </BoardOverlay>
     );
 };
 
