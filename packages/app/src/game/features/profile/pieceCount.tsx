a1fcdd11fdd3719415b5f437a238ea57868c77ab
James Monger <jameskmonger@hotmail.co.uk>
2021-04-11 16:00:00 +0100

    refactor: move ownedPieceCountSelector

--- packages/app/src/game/features/profile/pieceCount.tsx
+++ packages/app/src/game/features/profile/pieceCount.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import { useSelector } from "react-redux";
-import { AppState } from "../../../store";
-import { ownedPieceSelector } from "../../../store/pieceSelectors";
 import { getPlayerLevel } from "@creature-chess/shared";
+import { AppState } from "../../../store";
+import { ownedPieceCountSelector } from "./ownedPieceCountSelector";
 
 const PieceCount: React.FunctionComponent = props => {
   const level = useSelector<AppState, number>(getPlayerLevel);
-  const pieceCount = useSelector<AppState, number>(state => ownedPieceSelector(state).length);
+  const pieceCount = useSelector<AppState, number>(state => ownedPieceCountSelector(state));
 
   if (pieceCount !== level) {
     return <p className="item pieces warning">{pieceCount} / {level} pieces (board not full!)</p>;
