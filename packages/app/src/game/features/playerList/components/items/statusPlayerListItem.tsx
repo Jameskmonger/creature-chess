08d465b7179d59698f5d6a17d5e428b325937633
James Monger <jameskmonger@hotmail.co.uk>
2021-04-11 23:52:55 +0100

    refactor: split player list items

---
+++ packages/app/src/game/features/playerList/components/items/statusPlayerListItem.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { PlayerName } from "../playerName";
+import { BattleInfo } from "../battleInfo";
+
+const StatusPlayerListItem: React.FunctionComponent<{ playerId: string, status: string, subtitle?: string }> = ({ playerId, status, subtitle }) => {
+    return (
+        <div className="player-list-item quit">
+            <div className="half">
+                <span className="name"><PlayerName playerId={playerId} /></span>
+            </div>
+            <div className="half">
+                <span className="status">{status}</span>
+                <BattleInfo playerId={playerId} />
+                {subtitle && <span className="subtitle">{subtitle}</span>}
+            </div>
+        </div>
+    );
+};
+
+export { StatusPlayerListItem };
