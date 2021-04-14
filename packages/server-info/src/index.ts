98c47ecec103d94ca4fefd1d1f543e2c4a385de7
James Monger <jameskmonger@hotmail.co.uk>
2021-04-14 20:09:18 +0100

    fix: allow sentry-trace header

--- packages/server-info/src/index.ts
+++ packages/server-info/src/index.ts
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, sentry-trace");
     next();
 });
 
