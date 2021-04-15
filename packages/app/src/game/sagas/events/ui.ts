a6e3851607f67eb8e0df47eaf1f08506e1c7767b
James <james.hallam93@gmail.com>
2021-04-15 10:21:17 +0100

    import fix

--- packages/app/src/game/sagas/events/ui.ts
+++ packages/app/src/game/sagas/events/ui.ts
@@ -2,7 +2,7 @@ import { takeLatest, put, select } from "@redux-saga/core/effects";
 import { GamePhase } from "@creature-chess/models";
 import { clearSelectedPiece, openOverlay, closeOverlay, Overlay } from "../../../ui";
 import { gameRoundUpdateEvent, GameRoundUpdateEvent } from "./roundUpdate";
-import { AppState } from "packages/app/src/store";
+import { AppState } from "../../../store";
 
 export const uiSaga = function*() {
     yield takeLatest<GameRoundUpdateEvent>(
