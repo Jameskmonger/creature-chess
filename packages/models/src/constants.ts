6242dfea1abba948131934a2370f4cd07a8546c8
James <james.hallam93@gmail.com>
2021-05-11 16:54:42 +0100

    Lint: whitespace

--- packages/models/src/constants.ts
+++ packages/models/src/constants.ts
@@ -31,9 +31,9 @@ export const MAX_PLAYERS_IN_GAME = 8;
 export const LOBBY_WAIT_TIME = 90;
 
 export const AVAILABLE_PROFILE_PICTURES = {
-    1:"Budaye",
-    4:"Aardorn",
-    5:"Nut",
-    7:"Embra",
-    8:"Tweesher"
+    1: "Budaye",
+    4: "Aardorn",
+    5: "Nut",
+    7: "Embra",
+    8: "Tweesher"
 };
