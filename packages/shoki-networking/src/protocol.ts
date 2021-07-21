d5e6f0060237a02fafa669f7b56d7de61b532a9a
James Monger <jameskmonger@hotmail.co.uk>
2021-07-22 00:39:57 +0100

    refactor: add types to shoki protocol

--- packages/shoki-networking/src/protocol.ts
+++ packages/shoki-networking/src/protocol.ts
@@ -1,8 +1,17 @@
 import { PacketSet } from "./packet";
-import { incoming } from "./registry/incoming";
-import { outgoing } from "./registry/outgoing";
+import { incoming, IncomingRegistry, RegisterHandlerFn } from "./registry/incoming";
+import { outgoing, OutgoingRegistry, EmitFn } from "./registry/outgoing";
 
-export const protocol = <TPackets extends PacketSet>() => ({
+type OutgoingRegistryFactory<TPackets extends PacketSet> = (emit: EmitFn<TPackets>) => OutgoingRegistry<TPackets>;
+type IncomingRegistryFactory<TPackets extends PacketSet>
+	= (register: RegisterHandlerFn<TPackets>, unregister: RegisterHandlerFn<TPackets>) => IncomingRegistry<TPackets>;
+
+type ShokiProtocol<TPackets extends PacketSet> = {
+	outgoing: OutgoingRegistryFactory<TPackets>;
+	incoming: IncomingRegistryFactory<TPackets>;
+};
+
+export const protocol = <TPackets extends PacketSet>(): ShokiProtocol<TPackets> => ({
 	outgoing: outgoing<TPackets>(),
 	incoming: incoming<TPackets>()
 });
