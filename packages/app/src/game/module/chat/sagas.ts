bea1e9a83ecf0fe4f782427d4f340b8b69bcaa87
James <james.hallam93@gmail.com>
2021-07-24 17:25:10 +0100

    Fix: fixed handleQuickChat saga (added race)

--- packages/app/src/game/module/chat/sagas.ts
+++ packages/app/src/game/module/chat/sagas.ts
@@ -1,32 +1,27 @@
-import { take, delay, select, put } from "redux-saga/effects";
+import { take, delay, select, put, race } from "redux-saga/effects";
 import { commands as quickChatCommands } from "./state";
 import { PlayerEvents } from "@creature-chess/gamemode";
 
 
 export const handleQuickChat = function*() {
+	let action = yield take(PlayerEvents.playerReceiveQuickChatEvent.toString());
 	while (true) {
-		const action = yield take(PlayerEvents.playerReceiveQuickChatEvent.toString());
+
 		const { sendingPlayerId, receivingPlayerId, chatValue } = action.payload;
 		if (!chatValue) {
 			return;
 		}
 		yield put(quickChatCommands.setPlayerChat({ sendingPlayerId, receivingPlayerId, chatValue }));
-		let chatToCheck = chatValue;
-		while (true) {
-
-			yield delay(3000);
-			// stops chat being cleared before 3 seconds if it has changed in that time frame
-			// may be able to get rid of below using take latest?
-
-			const currentChat = yield select(state => state.game.quickChat.find(chatObject => chatObject.id === sendingPlayerId));
-			const isSameChat = currentChat?.value?.phrase === chatToCheck?.phrase;
-
-			if (!isSameChat) {
-				chatToCheck = currentChat?.value;
-				continue;
-			}
-			yield put(quickChatCommands.setPlayerChat({ sendingPlayerId, receivingPlayerId, chatValue: null }));
-			break;
+		const { newChat, timeout } = yield race({
+			newChat: take(PlayerEvents.playerReceiveQuickChatEvent.toString()),
+			timeout: delay(3000)
+		});
+		if (newChat) {
+			action = newChat;
+			continue;
 		}
+		yield put(quickChatCommands.setPlayerChat({ sendingPlayerId, receivingPlayerId, chatValue: null }));
+
+		action = yield take(PlayerEvents.playerReceiveQuickChatEvent.toString());
 	}
 };
