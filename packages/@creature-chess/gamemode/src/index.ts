795e0ad0f2a5c1be9ac41d7c935fd542b5356506
James <james.hallam93@gmail.com>
2021-08-03 16:11:33 +0100

    Exports from other packages

--- packages/@creature-chess/gamemode/src/index.ts
+++ packages/@creature-chess/gamemode/src/index.ts
@@ -7,15 +7,16 @@ export {
 	PlayerEvents, PlayerVariables
 } from "./entities/player";
 export { PlayerInfoState, playerInfoReducer, PlayerMatchRewards } from "./entities/player/state/playerInfo";
-
+export { PlayerInfoUpdateCommand } from "./entities/player/state/commands";
 export { PlayerAction, PlayerActionTypesArray } from "./playerActions";
 export * as PlayerActions from "./playerActions";
 
+export { CardDeck } from "./game/cardDeck";
 export { getPiece, getAllPieces } from "./player/pieceSelectors";
 export { getPlayerLevel, getPlayerMoney, getPlayerXp, isPlayerAlive } from "./entities/player/state/selectors";
 
 export { roundInfoReducer, RoundInfoCommands } from "./game/roundInfo";
 export * as GameEvents from "./game/events";
-export { getDefinitionById } from "./definitions";
+export { getDefinitionById, getAllDefinitions } from "./definitions";
 
 export { Match } from "./game/match";
