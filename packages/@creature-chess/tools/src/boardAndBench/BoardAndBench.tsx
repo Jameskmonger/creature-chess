6b950b84fd3fd90d59a2b13cc85589fbbfd0347e
James <james.hallam93@gmail.com>
2021-08-06 17:17:49 +0100

    Feat: Added bench and board

--- packages/@creature-chess/tools/src/boardAndBench/BoardAndBench.tsx
+++ packages/@creature-chess/tools/src/boardAndBench/BoardAndBench.tsx
@@ -2,26 +2,35 @@ import { CardShop } from "./cardShop/CardShop";
 import { Card as CardModel, CreatureType, DefinitionClass } from "@creature-chess/models";
 import React from "react";
 import { Layout } from "@creature-chess/ui/lib/layout";
+import { DevBoard } from "./board/DevBoard";
+import { DndProvider } from "react-dnd";
+import MultiBackend from "react-dnd-multi-backend";
+import HTML5toTouch from "react-dnd-multi-backend/dist/esm/HTML5toTouch";
+
+import "./_board.scss";
+import { DevBench } from "./bench/DevBench";
 
 
 const BoardAndBench: React.FunctionComponent = () => {
 	const money = 100;
 
 	return (
-		<Layout
-			className="board-and-bench"
-			direction="column"
-		>
-			<h3>UserInterface</h3>
-			<p>board</p>
-			<p>bench</p>
+		<DndProvider backend={MultiBackend} options={HTML5toTouch}>
 
-			<div className="card-shop-container">
-				<CardShop
-					devMode={true}
-				/>
-			</div>
-		</Layout>
+			<Layout
+				className="board-and-bench"
+				direction="column"
+			>
+				<h3>UserInterface</h3>
+				<DevBoard />
+				<DevBench />
+				<div className="card-shop-container">
+					<CardShop
+						devMode={true}
+					/>
+				</div>
+			</Layout>
+		</DndProvider>
 	);
 };
 
