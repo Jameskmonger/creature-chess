35d7d1aaed90ee055b4bdc6862f6a98cd5c96fd3
jameshallam93 <63243789+jameshallam93@users.noreply.github.com>
2021-09-23 21:20:24 +0100

    Update shouldBuyXp.ts + buyCard.ts

--- packages/@creature-chess/server-game/src/player/bot/preparingPhase/shouldBuyXp.ts
+++ packages/@creature-chess/server-game/src/player/bot/preparingPhase/shouldBuyXp.ts
@@ -1,10 +1,14 @@
 import { BUY_XP_AMOUNT, BUY_XP_COST, MAX_PLAYER_LEVEL, getXpToNextLevel } from "@creature-chess/models";
 import { PlayerState } from "@creature-chess/gamemode";
-
+import { BoardSelectors } from "@shoki/board";
 // don't go under this amount
 const MINIMUM_MONEY = 10;
+const MINIMUM_MONEY_IF_NEXT_LEVEL_PRACTICAL = 5;
 
 export const shouldBuyXp = (state: PlayerState): boolean => {
+
+	const benchPieces = BoardSelectors.getAllPieces(state.bench);
+	const hasPieceOnBench = benchPieces.length >= 1;
 	const { playerInfo: { money, level, xp } } = state;
 
 	if (level === MAX_PLAYER_LEVEL) {
@@ -20,6 +24,10 @@ export const shouldBuyXp = (state: PlayerState): boolean => {
 	const xpRequired = xpForNextLevel - xp;
 	const purchasesRequired = Math.ceil(xpRequired / BUY_XP_AMOUNT);
 	const cost = purchasesRequired * BUY_XP_COST;
+
+	if (purchasesRequired === 1 && hasPieceOnBench) {
+		return (money - cost) >= MINIMUM_MONEY_IF_NEXT_LEVEL_PRACTICAL;
+	}
 
 	return (money - cost) >= MINIMUM_MONEY;
 };
