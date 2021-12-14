468a147cbc6056fc439e13de64abacf110ef46d3
James Monger <jameskmonger@hotmail.co.uk>
2021-12-14 02:27:35 +0000

    serve web-game from server-game

--- apps/server-game/index.ts
+++ apps/server-game/index.ts
@@ -1,14 +1,26 @@
 import { createServer } from "http";
+import path from "path";
+import { existsSync } from "fs";
+import express from "express";
 import { Server } from "socket.io";
 import { logger } from "./src/log";
 import { startServer } from "./src/server";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 
-const httpServer = createServer();
-const io = new Server(httpServer, { /* options */ });
+const publicPath = path.join(__dirname, "/public");
+if (!existsSync(publicPath)) {
+	logger.error(`No /public dir found (looked in ${publicPath})`);
+	process.exit(1);
+}
 
-httpServer.listen(port, "0.0.0.0");
-logger.info(`Server running on port ${port}`);
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.use(express.static(publicPath));
 
 startServer({ io });
+
+server.listen(port, "0.0.0.0");
+logger.info(`Server running on port ${port}`);
