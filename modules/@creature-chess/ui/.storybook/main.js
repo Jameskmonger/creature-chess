7fb9387fb8d287e7e31ac1c29625baf105eff580
James Monger <jameskmonger@hotmail.co.uk>
2022-06-08 19:12:10 +0100

    add preview styling (100% width/height)

--- modules/@creature-chess/ui/.storybook/main.js
+++ modules/@creature-chess/ui/.storybook/main.js
@@ -1,9 +1,18 @@
 module.exports = {
-	"stories": [
-		"../../../../**/*.stories.@(js|jsx|ts|tsx)"
-	],
-	"addons": [
-		"@storybook/addon-links",
-		"@storybook/addon-essentials"
-	]
-}
+  stories: ["../../../../**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  previewHead: (head) => `
+		${head}
+		<style>
+		body {
+			padding: 0 !important;
+			height: 100vh !important;
+		}
+
+		#root {
+			height: 100%;
+			width: 100%;
+		}
+		</style>
+	`,
+};
