4c287c97123254747063d1a0cf44a36479905304
James Monger <jameskmonger@hotmail.co.uk>
2022-06-08 20:42:18 +0100

    remove old classes

--- modules/@creature-chess/ui/src/playerList/playerListItem.tsx
+++ modules/@creature-chess/ui/src/playerList/playerListItem.tsx
@@ -83,9 +83,7 @@ const PlayerListItem: React.FunctionComponent<Props> = (props) => {
 			ref={ref as any}
 		>
 			<Layout direction="row" noSpacer>
-				<div className="picture">
-					<PlayerAvatar player={player} />
-				</div>
+				<PlayerAvatar player={player} />
 				<div className={classes.details}>
 					<Layout direction="row" noSpacer>
 						<Half><PlayerProfile position={index + 1} player={player} isLocal={isLocal} /></Half>
