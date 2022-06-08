089f4026dc8668fe2f45055cadb301b85d851aec
James Monger <jameskmonger@hotmail.co.uk>
2022-06-08 19:59:06 +0100

    use useStyles in Ready button

--- apps/web-game/src/game/board/overlays/readyUpButton.tsx
+++ apps/web-game/src/game/board/overlays/readyUpButton.tsx
@@ -3,9 +3,26 @@ import { useSelector, useDispatch } from "react-redux";
 import { AppState } from "../../../store";
 import { GamePhase } from "@creature-chess/models";
 import { PlayerActions } from "@creature-chess/gamemode";
+import { createUseStyles } from "react-jss";
+
+const COLOR_READY_BUTTON = "#38b764";
+const COLOR_READY_BUTTON_TEXT = "#000000";
+
+const useStyles = createUseStyles({
+	readyButton: {
+		"box-sizing": "border-box",
+		"padding": "2rem",
+		"font-size": "1.6rem",
+		"color": COLOR_READY_BUTTON_TEXT,
+		"cursor": "pointer",
+		"background": COLOR_READY_BUTTON,
+		"border": "none",
+	}
+})
 
 const ReadyUpButton: React.FC = () => {
 	const dispatch = useDispatch();
+	const styles = useStyles();
 
 	const inPreparingPhase = useSelector<AppState, boolean>(state => state.game.roundInfo.phase === GamePhase.PREPARING);
 	const notReady = useSelector<AppState, boolean>(state => state.game.playerInfo.ready === false);
@@ -18,7 +35,7 @@ const ReadyUpButton: React.FC = () => {
 
 	const onReadyUp = () => dispatch(PlayerActions.readyUpPlayerAction());
 
-	return <button className="ready-up" onClick={onReadyUp}>Ready</button>;
+	return <button className={styles.readyButton} onClick={onReadyUp}>Ready</button>;
 };
 
 export { ReadyUpButton };
