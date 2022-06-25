dcdabfba7333ef8b557917015c2ad8177639ea6b
James Monger <jameskmonger@hotmail.co.uk>
2022-06-25 12:02:17 +0100

    feat: log unhandled rejections

--- apps/server-game/index.ts
+++ apps/server-game/index.ts
@@ -7,12 +7,22 @@ import { logger } from "./src/log";
 import { logger as expressWinston } from "express-winston";
 import { startServer } from "./src/server";
 
+process.on("unhandledRejection", function (reason, p) {
+  console.log(
+    "Possibly Unhandled Rejection at: Promise ",
+    p,
+    " reason: ",
+    reason
+  );
+  // application specific logging here
+});
+
 const port = parseInt(process.env.PORT || "3000", 10);
 
 const publicPath = path.join(__dirname, "/public");
 if (!existsSync(publicPath)) {
-	logger.error(`No /public dir found (looked in ${publicPath})`);
-	process.exit(1);
+  logger.error(`No /public dir found (looked in ${publicPath})`);
+  process.exit(1);
 }
 
 const app = express();
