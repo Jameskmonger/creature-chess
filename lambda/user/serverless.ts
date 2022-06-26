38c34dc6fa6871c885c45a95dc7b817d92e8df1a
James Monger <jameskmonger@hotmail.co.uk>
2022-06-26 17:36:17 +0100

    chore: serverless create

---
+++ lambda/user/serverless.ts
@@ -0,0 +1,37 @@
+import hello from "@functions/hello";
+import type { AWS } from "@serverless/typescript";
+
+const serverlessConfiguration: AWS = {
+	service: "user",
+	frameworkVersion: "3",
+	plugins: ["serverless-esbuild"],
+	provider: {
+		name: "aws",
+		runtime: "nodejs14.x",
+		apiGateway: {
+			minimumCompressionSize: 1024,
+			shouldStartNameWithService: true,
+		},
+		environment: {
+			AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+			NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+		},
+	},
+	// import the function via paths
+	functions: { hello },
+	package: { individually: true },
+	custom: {
+		esbuild: {
+			bundle: true,
+			minify: false,
+			sourcemap: true,
+			exclude: ["aws-sdk"],
+			target: "node14",
+			define: { "require.resolve": undefined },
+			platform: "node",
+			concurrency: 10,
+		},
+	},
+};
+
+module.exports = serverlessConfiguration;
