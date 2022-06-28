e4fa917ef5bf1b9af6d0968c49a73ad8d2e40b9b
James Monger <jameskmonger@hotmail.co.uk>
2022-06-28 22:20:14 +0100

    prettier fix

--- lambda/user/src/libs/lambda.ts
+++ lambda/user/src/libs/lambda.ts
@@ -1,6 +1,6 @@
-import middy from "@middy/core"
-import middyJsonBodyParser from "@middy/http-json-body-parser"
+import middy from "@middy/core";
+import middyJsonBodyParser from "@middy/http-json-body-parser";
 
 export const middyfy = (handler) => {
-  return middy(handler).use(middyJsonBodyParser())
-}
+	return middy(handler).use(middyJsonBodyParser());
+};
