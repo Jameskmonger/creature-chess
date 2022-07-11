56c3cd4ecf4aa3bde7f3a65d573e69146cb1cdec
James Monger <jameskmonger@hotmail.co.uk>
2022-07-11 12:49:34 +0100

    update ecr push

--- scripts/ecr-push.sh
+++ scripts/ecr-push.sh
@@ -1,7 +1,9 @@
-docker build -t cc-server-game-nginx -f nginx/server-game/Dockerfile .
-docker tag cc-server-game-nginx:latest 352494607378.dkr.ecr.eu-west-2.amazonaws.com/cc-server-game-nginx:latest
-docker push 352494607378.dkr.ecr.eu-west-2.amazonaws.com/cc-server-game-nginx:latest
+docker login --username AWS -p $(aws ecr get-login-password --region eu-west-1) 522702119152.dkr.ecr.eu-west-1.amazonaws.com
 
-docker build -t cc-server-game -f apps/Dockerfile --build-arg APP_DIR=server-game .
-docker tag cc-server-game:latest 352494607378.dkr.ecr.eu-west-2.amazonaws.com/cc-server-game:latest
-docker push 352494607378.dkr.ecr.eu-west-2.amazonaws.com/cc-server-game:latest
+docker build -t server-game-nginx -f nginx/server-game/Dockerfile .
+docker tag server-game-nginx:latest 522702119152.dkr.ecr.eu-west-1.amazonaws.com/server-game-nginx:latest
+docker push 522702119152.dkr.ecr.eu-west-1.amazonaws.com/server-game-nginx:latest
+
+docker build -t server-game -f apps/Dockerfile --build-arg APP_DIR=server-game .
+docker tag server-game:latest 522702119152.dkr.ecr.eu-west-1.amazonaws.com/server-game:latest
+docker push 522702119152.dkr.ecr.eu-west-1.amazonaws.com/server-game:latest
