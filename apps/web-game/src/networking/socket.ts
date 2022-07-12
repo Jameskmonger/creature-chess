897b96b0e91295ca5319ef03061fdb832dc16bad
James Monger <jameskmonger@hotmail.co.uk>
2022-07-12 23:16:18 +0100

    chore: update socket.io

--- apps/web-game/src/networking/socket.ts
+++ apps/web-game/src/networking/socket.ts
@@ -3,13 +3,13 @@ import { io, Socket } from "socket.io-client";
 import { GameServerToClient } from "@creature-chess/networking";
 
 export const getSocket = (idToken: string) => {
-	const socket = io(
+	const socket = (io as any)(
 		{
 			path: "/game/socket.io",
 		},
 		{
 			// use websocket first if available
-			transports: ["websocket", "polling"],
+			transports: ["websocket"],
 		}
 	);
 
