f028a786f6c3bc0525e78709288fd48e20703e08
James <james.hallam93@gmail.com>
2022-07-14 23:20:09 +0100

    fix: :bug: Stop default returning to live site when quitting game

--- apps/web-game/src/game/sagas/goToMenuAfterGame.ts
+++ apps/web-game/src/game/sagas/goToMenuAfterGame.ts
@@ -1,11 +1,12 @@
 import { delay, take } from "@redux-saga/core/effects";
 
 import { GameEvents } from "@creature-chess/gamemode";
+import { config } from "@creature-chess/models";
 
 export const goToMenuAfterGame = function* () {
 	yield take<GameEvents.GameFinishEvent>(GameEvents.gameFinishEvent.toString());
 
 	yield delay(10_000);
 
-	window.location.href = "https://creaturechess.com";
+	window.location.href = config.appUrl;
 };
