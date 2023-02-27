4722ed7f83500146d266216d25133f2810b74f86
James Monger <jameskmonger@hotmail.co.uk>
2023-02-27 18:59:33 +0000

    feat: create getByAuthId

--- modules/@creature-chess/data/src/user/_setup.ts
+++ modules/@creature-chess/data/src/user/_setup.ts
@@ -5,25 +5,8 @@ import { COLLECTION_NAMES, INDEX_NAMES } from "../constants";
 
 const INSTANCE_ALREADY_EXISTS = "instance already exists";
 
-export const setupUserDatabase = async (
-	client: FaunaDBClient
-): Promise<boolean> => {
+async function createIndices(client: FaunaDBClient) {
 	let changesMade = false;
-
-	try {
-		await client.query(
-			q.CreateCollection({
-				name: COLLECTION_NAMES.USERS,
-			})
-		);
-
-		console.log(` - Created collection '${COLLECTION_NAMES.USERS}'`);
-		changesMade = true;
-	} catch (e: any) {
-		if (e.message !== INSTANCE_ALREADY_EXISTS) {
-			throw e;
-		}
-	}
 
 	try {
 		await client.query(
@@ -51,5 +34,56 @@ export const setupUserDatabase = async (
 		}
 	}
 
+	try {
+		await client.query(
+			q.CreateIndex({
+				name: INDEX_NAMES.USERS_BY_AUTH_ID,
+				serialized: true,
+				unique: true,
+				source: q.Collection(COLLECTION_NAMES.USERS),
+				terms: [
+					{
+						// todo type safety around this - is it even possible?
+						field: ["data", "authId"],
+					},
+				],
+			})
+		);
+
+		console.log(
+			` - Created index '${INDEX_NAMES.USERS_BY_AUTH_ID}'`
+		);
+		changesMade = true;
+	} catch (e: any) {
+		if (e.message !== INSTANCE_ALREADY_EXISTS) {
+			throw e;
+		}
+	}
+
 	return changesMade;
+}
+
+export const setupUserDatabase = async (
+	client: FaunaDBClient
+): Promise<boolean> => {
+	let changesMade = false;
+
+	try {
+		await client.query(
+			q.CreateCollection({
+				name: COLLECTION_NAMES.USERS,
+			})
+		);
+
+		console.log(` - Created collection '${COLLECTION_NAMES.USERS}'`);
+		changesMade = true;
+	} catch (e: any) {
+		if (e.message !== INSTANCE_ALREADY_EXISTS) {
+			throw e;
+		}
+	}
+
+	const indicesCreated = await createIndices(client);
+
+	return changesMade || indicesCreated;
 };
