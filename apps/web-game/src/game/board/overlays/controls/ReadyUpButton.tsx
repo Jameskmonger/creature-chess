1314340f04e9eb699fd4a4530c6f318fc5cc4d92
James Monger <jameskmonger@hotmail.co.uk>
2023-02-27 21:55:24 +0000

    fix: add dependency to useCallback

--- apps/web-game/src/game/board/overlays/controls/ReadyUpButton.tsx
+++ apps/web-game/src/game/board/overlays/controls/ReadyUpButton.tsx
@@ -39,7 +39,7 @@ export function ReadyUpButton() {
 		}
 
 		dispatch(PlayerActions.readyUpPlayerAction());
-	}, []);
+	}, [canReadyUp]);
 
 	if (!canReadyUp) {
 		// To keep the Sell button in the same place
