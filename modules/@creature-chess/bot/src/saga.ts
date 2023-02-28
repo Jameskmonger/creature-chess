480b8167c89fde21f4ec404670d8663d4cf4d534
James Monger <jameskmonger@hotmail.co.uk>
2023-02-28 21:33:00 +0000

    fix: stop bots from running when dead

--- modules/@creature-chess/bot/src/saga.ts
+++ modules/@creature-chess/bot/src/saga.ts
@@ -1,21 +1,27 @@
 import delay from "delay";
-import { all, call, takeLatest, put } from "redux-saga/effects";
+import { all, call, takeLatest, put, select } from "typed-redux-saga";
 
 import { BotPersonality } from "@creature-chess/data";
-import { PlayerEvents, GameEvents } from "@creature-chess/gamemode";
+import { PlayerEvents, GameEvents, PlayerState } from "@creature-chess/gamemode";
 import { GamePhase } from "@creature-chess/models";
 
 import { preparingPhase } from "./preparingPhase";
 import { putBenchOnBoard } from "./putBenchOnBoard";
 
-export const botLogicSaga = function* (personality: BotPersonality) {
+export const botLogicSaga = function*(personality: BotPersonality) {
 	yield all([
 		call(putBenchOnBoard),
 		takeLatest<GameEvents.GamePhaseStartedEvent>(
 			GameEvents.gamePhaseStartedEvent.toString(),
-			function* ({ payload: { phase } }) {
+			function*({ payload: { phase } }) {
 				// delay all events to prevent any timing issues
 				// todo improve this.. expose some event that happens after all the "game logic" has happened
+				const state: PlayerState = yield select();
+
+				if (state.playerInfo.health <= 0) {
+					return;
+				}
+
 				yield delay(1000);
 
 				if (phase === GamePhase.PREPARING) {
