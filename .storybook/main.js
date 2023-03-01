2a585947a69205fc550f5abad2215df08c4b6c65
James Monger <jameskmonger@hotmail.co.uk>
2023-03-01 17:39:02 +0000

    chore: move storybook to root of repo

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-	stories: ["../../../../**/*.stories.@(js|jsx|ts|tsx)"],
+	stories: ["../**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 	previewHead: (head) => `
 		${head}
