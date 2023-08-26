0b3fce21c54dd6605581bd1a8455c54b34bee6b7
James Monger <jameskmonger@hotmail.co.uk>
2023-08-26 08:04:40 +0000

    refactor: query bots through prisma directly

--- apps/server-game/src/external/bots.ts
+++ apps/server-game/src/external/bots.ts
@@ -4,13 +4,19 @@ import { LobbyPlayer } from "@creature-chess/models";
 export const getBots = async (database: DatabaseConnection, count: number) => {
 	const output: { player: LobbyPlayer; personality: BotPersonality }[] = [];
 
-	const bots = await database.bot.getLeastPlayedBots(count);
-	for (const { id, nickname, ambition, composure, vision } of bots!) {
+	const bots = await database.prisma.bots.findMany({
+		take: count,
+		orderBy: {
+			games_played: "asc",
+		},
+	});
+
+	for (const { id, nickname, ambition, composure, vision } of bots) {
 		// get a random picture from one to 20 - temporary
 		const picture = Math.floor(Math.random() * 20) + 1;
 
 		const player = {
-			id: (id + 1) * -1,
+			id: "bot-" + id,
 			name: `[BOT] ${nickname}`,
 			profile: {
 				title: null,
