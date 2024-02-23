d0640ab3081739c948d97b12befe8b6fd63002f8
James Monger <jameskmonger@hotmail.co.uk>
2024-02-23 23:43:31 +0000

    feat: create generic Button component

---
+++ modules/@cc-web/ui/components/Button.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "./Button";
+
+const meta: Meta<typeof Button> = {
+	title: "@cc-web / ui / components / Button",
+	component: Button,
+};
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+	args: {
+		children: "Button",
+		size: "medium",
+		type: "primary",
+	},
+};
+
+export const Secondary: Story = {
+	args: {
+		children: "Button",
+		size: "medium",
+		type: "secondary",
+	},
+};
