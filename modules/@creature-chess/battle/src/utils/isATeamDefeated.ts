9412581f91eaf02605979e31fa3d417e89b0c76e
James Monger <jameskmonger@hotmail.co.uk>
2024-02-24 03:52:19 +0000

    feat: check for living pieces in isATeamDefeated

--- modules/@creature-chess/battle/src/utils/isATeamDefeated.ts
+++ modules/@creature-chess/battle/src/utils/isATeamDefeated.ts
@@ -3,9 +3,9 @@ import { BoardSelectors, BoardState } from "@shoki/board";
 import { PieceModel } from "@creature-chess/models";
 
 export const isATeamDefeated = (board: BoardState<PieceModel>) => {
-	const pieceOwnerIds = BoardSelectors.getAllPieces(board).map(
-		(p) => p.ownerId
-	);
+	const pieceOwnerIds = BoardSelectors.getAllPieces(board)
+		.filter((p) => p.currentHealth > 0)
+		.map((p) => p.ownerId);
 
 	// if there are only pieces belonging to 1 player, then we have a winner
 	return new Set(pieceOwnerIds).size === 1;
