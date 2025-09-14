130ba1fe5e19a626c3ac8443b9743518ee820e1c
James Monger <jameskmonger@hotmail.co.uk>
2025-09-15 00:44:33 +0100

    refactor: use `Parameters` to remove `CreateUseStylesOptions`

--- apps/web-game/src/useStyles.ts
+++ apps/web-game/src/useStyles.ts
@@ -1,28 +1,15 @@
-import type { StyleSheetFactoryOptions } from "jss";
-import { Styles, Theming, createUseStyles } from "react-jss";
+import { createUseStyles } from "react-jss";
 
 type Theme = {
 	background: string;
 };
 
-/**
- * This type isn't exported from react-jss, but we
- * need it to create our own useStyles hook.
- */
-interface CreateUseStylesOptions extends StyleSheetFactoryOptions {
-	index?: number;
-	theming?: Theming<Theme>;
-	name?: string;
-}
-
 export function createUseThemeStyles<
 	C extends string = string,
 	Props = unknown,
 >(
-	styles:
-		| Styles<C, Props, Theme>
-		| ((theme: Theme) => Styles<C, Props, undefined>),
-	options?: CreateUseStylesOptions
+	styles: Parameters<typeof createUseStyles<C, Props, Theme>>[0],
+	options?: Parameters<typeof createUseStyles<C, Props, Theme>>[1]
 ) {
 	return createUseStyles<C, Props, Theme>(styles, options);
 }
