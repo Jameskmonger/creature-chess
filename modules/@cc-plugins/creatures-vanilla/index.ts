1bec68c6d48c5053433e13de7f8fc35a0703fae7
james <jameskmonger@hotmail.co.uk>
2026-06-25 02:07:16 +0100

    chore: export vanilla creatures for non-plugin imports

--- modules/@cc-plugins/creatures-vanilla/index.ts
+++ modules/@cc-plugins/creatures-vanilla/index.ts
@@ -1,2 +1,6 @@
 // Server entry - the kernel loader's require() lands here.
 export { default } from "./src/index";
+
+// Raw catalog, for tooling that needs the definitions outside a running plugin
+// (e.g. offline encoders/analysers keyed on the vanilla creature set).
+export { vanillaCreatureDefinitions } from "./src/definitions";
